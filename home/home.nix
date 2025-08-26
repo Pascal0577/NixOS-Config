cf9f8c7bb7b40da007a7aa935fbf85cf0762e078
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2025-08-26 02:01:56 -0500

    removed redundant code

--- home/home.nix
+++ home/home.nix
@@ -20,10 +20,7 @@
 
   home.stateVersion = "25.05";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
-    ghostty
     celluloid
   ];
 
