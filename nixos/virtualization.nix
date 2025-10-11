d4b7f9593580e6b31fa3f90706195cc133b824cf
Pascal0577 <pascalthederg@gmail.com>
2025-10-11 15:11:33 -0500

    updates to virtualization.nix if I want to do GPU passhtrough

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -1,13 +1,38 @@
-{ username, ... }:
+{ username, pkgs, ... }:
 
 {
     programs.virt-manager.enable = true;
 
     users.groups.libvirtd.members = ["${username}"];
 
-    virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd = {
+        enable = true;
+        qemu = {
+            package = pkgs.qemu_kvm;
+            swtpm.enable = true;  # TPM emulation for Windows 11
+        };
+    };
 
     virtualisation.spiceUSBRedirection.enable = true;
 
     users.users.${username}.extraGroups = [ "libvirtd" ];
+
+    # boot = {
+    #     blacklistedKernelModules = [ "nouveau" "nvidia" ];
+
+    #     initrd.kernelModules = [
+    #         "vfio_pci"
+    #         "vfio"
+    #         "vfio_iommu_type1"
+    #     ];            
+
+    #     kernelParams = [
+    #         "intel_iommu=on"
+    #         "iommu=pt"
+    #     ];
+
+    #     extraModprobeConfig = ''
+    #         options vfio-pci ids=10de:28e1,10de:22be
+    #     '';
+    # };
 }
