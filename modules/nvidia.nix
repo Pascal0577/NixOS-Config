0ca53b9617897b2266570656aacc97eb30b18f15
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2025-11-05 00:52:23 -0600

    changed kernel parameters for slightly better obs performance

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -18,8 +18,6 @@
 
         hardware.graphics.extraPackages = with pkgs; [
             intel-media-driver 
-            intel-vaapi-driver 
-            libvdpau-va-gl
         ];
 
         boot = {
@@ -29,8 +27,8 @@
 
             kernelParams = [
                 "i915.fastboot=1"
-                "i915.enable_guc=3"
-                "i915.enable_fbc=3"
+                "i915.enable_guc=0"
+                "i915.enable_fbc=0"
             ];
         };
     };
