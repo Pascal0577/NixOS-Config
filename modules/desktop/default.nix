9370129e664b3305a2007cdead6cc739acedf1ed
Pascal0577 <pascalthederg@gmail.com>
2025-11-14 16:08:30 -0600

    added flatpak so I could play seihou

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -11,6 +11,15 @@
         ./walker.nix
     ];
 
+    services.flatpak.enable = true;
+    systemd.services.flatpak-repo = {
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.flatpak ];
+      script = ''
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      '';
+    };
+
     environment.systemPackages = with pkgs; [
         playerctl
         losslesscut-bin
