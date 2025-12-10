7c3f0dfc09a79fe13d8f1b9eb4ce8a02b625e47f
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2025-12-09 18:26:57 -0600

    removed flatpak

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -11,15 +11,6 @@
         ./walker.nix
     ];
 
-    services.flatpak.enable = true;
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
-
     environment.systemPackages = with pkgs; [
         playerctl
         losslesscut-bin
