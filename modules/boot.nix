772c69c5dd461155028090af900d63b6ea572fda
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2025-12-15 23:24:13 -0500

    re-added secure boot message

--- modules/boot.nix
+++ modules/boot.nix
@@ -10,7 +10,6 @@
             theme = "bgrt";
         };
 
-
         lanzaboote = {
             enable = true;
             pkiBundle = "/var/lib/sbctl/";
@@ -40,3 +39,11 @@
         };
     };
 }
+
+# More work needs to be done if I want to enable secure boot.
+# Here's a quick summary of what I need to do:
+# sudo nix-shell -p sbctl
+# sudo sbctl create-keys
+# Clear all the secure boot keys in the UEFI and enable secure boot there
+# sudo sbctl enroll-keys --microsoft
+# Re-enable secure boot in UEFI if needed
