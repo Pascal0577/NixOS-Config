11590b78d3201d761775166b7d4e9f81490f59f8
Pascal0577 <pascalthederg@gmail.com>
2026-01-01 16:08:13 -0500

    moved paperwm to its own extension

--- modules/desktop/gnome/default.nix
+++ modules/desktop/gnome/default.nix
@@ -1,6 +1,10 @@
-{ pkgs, username, config, lib, ... }:
+{ pkgs, username, ... }:
 
 {
+    imports = [
+        # ./paperwm.nix
+    ];
+
     environment.systemPackages = with pkgs; [
         yaru-theme
         icoextract
@@ -45,7 +49,6 @@
                     disable-user-extensions = false;
                     enabled-extensions = with pkgs.gnomeExtensions; [
                         user-themes.extensionUuid
-                        paperwm.extensionUuid
                     ];
                     favorite-apps = [
                         # .desktop files can be found in "/run/current-system/sw/share/applications/"
@@ -70,83 +73,9 @@
                     ];
                 };
 
-                "org/gnome/shell/extensions/paperwm" = {
-                    animation-time = 0.2;
-                    default-focus-mode = 0; # Edge, vs center or default
-                    disable-topbar-styling = true;
-                    edge-preview-enable = true;
-                    edge-preview-scale = 0.25;
-                    edge-preview-timeout = 0;
-                    edge-preview-timeout-enable = true;
-                    horizontal-margin = 10;
-                    vertical-margin = 10;
-                    vertical-margin-bottom = 10;
-                    window-gap = 10;
-                    use-default-background = true;
-                    show-workspace-indicator = false;
-                    show-window-position-bar = false;
-                    show-open-position-icon = true;
-                    show-focus-mode-icon = true;
-                    selection-border-size = 5;
-                    selection-border-radius-top = 10;
-                    selection-border-radius-bottom = 10;
-                };
-
-                "org/gnome/shell/extensions/paperwm/keybindings" = {
-                    close-window = ["<Super>q"];
-                    move-left = [
-                        "<Shift><Super>Left"
-                        "<Shift><Super>a"
-                    ];
-                    move-right = [
-                        "<Shift><Super>Right"
-                        "<Shift><Super>d"
-                    ];
-                    move-up = [
-                        "<Control><Super>Up"
-                        "<Control><Super>w"
-                    ];
-                    move-down = [
-                        "<Control><Super>Down"
-                        "<Control><Super>s"
-                    ];
-                    switch-left = [
-                        "<Super>Left"
-                        "<Super>a"
-                    ];
-                    switch-right = [
-                        "<Super>Right"
-                        "<Super>d"
-                    ];
-                    move-down-workspace = [
-                        "<Shift><Super>Down"
-                        "<Shift><Super>s"
-                    ];
-                    move-up-workspace = [
-                        "<Shift><Super>Up"
-                        "<Shift><Super>w"
-                    ];
-                    move-to-workspace-down = [
-                        "<Super>Down"
-                        "<Super>s"
-                    ];
-                    move-to-workspace-up = [
-                        "<Super>Up"
-                        "<Super>w"
-                    ];
-                    switch-monitor-above = [""];
-                    switch-monitor-below = [""];
-                    switch-monitor-left = [""];
-                    switch-monitor-right = [""];
-                };
-
                 "org/gnome/desktop/interface" = {
                     show-battery-percentage = true;
                     accent-color = "pink";
-                    font-antialiasing = "standard";
-                    font-hinting = "full";
-                    font-name = "Ubuntu Sans 12";
-                    monospace-font-name = lib.mkDefault "Ubuntu Sans Mono 12";
                     clock-format = "12h";
                 };
 
