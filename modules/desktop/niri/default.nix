dcbfb49e713c7ea700db684772356a3941acc82f
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-01-01 19:12:15 -0500

    using gnome polkit agent

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -36,6 +36,20 @@ in
         package = niri-optimized;
     };
 
+    systemd.user.services.niri-flake-polkit = {
+        description = "PolicyKit Authentication Agent provided by niri-flake";
+        wantedBy = [ "niri.service" ];
+        after = [ "graphical-session.target" ];
+        partOf = [ "graphical-session.target" ];
+        serviceConfig = {
+            Type = "simple";
+            ExecStart = lib.mkForce "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+        };
+    };
+
     environment.systemPackages = with pkgs; [
         nautilus
         baobab
