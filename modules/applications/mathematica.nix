e8ac23c6bb199e0c4bbb817e40056bb677bd2037
Pascal0577 <pascalthederg@gmail.com>
2026-01-07 23:59:00 -0500

    updated flake.lock

--- modules/applications/mathematica.nix
+++ modules/applications/mathematica.nix
@@ -4,8 +4,7 @@ let
         source = pkgs.requireFile {
             name = "Wolfram_14.3.0_LIN.sh";
             # Get this hash via a command similar to this:
-            # nix-store --query --hash \
-            # $(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica_XX.X.X_BNDL_LINUX.sh')
+            # nix-store --query --hash $(nix store add-path Wolfram_14.3.0_LIN.sh --name 'Wolfram_14.3.0_LIN.sh')
             sha256 = "18a8wf9j6n4dp631psa254hyniklm61i3qqrvvnz1sgfvd1hl6cf";
             message = ''
                 Your override for Mathematica includes a different src for the installer,
