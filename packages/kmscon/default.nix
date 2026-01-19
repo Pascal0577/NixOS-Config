24731fc25cc4047c429b024b57632806ea03a1e4
Pascal0577 <pascalthederg@gmail.com>
2026-01-19 18:18:16 -0500

    formatted

--- packages/kmscon/default.nix
+++ packages/kmscon/default.nix
@@ -1,81 +1,81 @@
-{
-    lib,
-    stdenv,
-    fetchFromGitHub,
-    meson,
-    systemdLibs,
-    libxkbcommon,
-    libdrm,
-    libGLU,
-    libGL,
-    pango,
-    pixman,
-    pkg-config,
-    docbook_xsl,
-    libxslt,
-    libgbm,
-    ninja,
-    check,
-    buildPackages,
-    pkgs,
+{ lib
+, stdenv
+, fetchFromGitHub
+, meson
+, systemdLibs
+, libxkbcommon
+, libdrm
+, libGLU
+, libGL
+, pango
+, pixman
+, pkg-config
+, docbook_xsl
+, libxslt
+, libgbm
+, ninja
+, check
+, buildPackages
+, pkgs
+,
 }:
 
 let
-    libtsm' = pkgs.callPackage ../libtsm/default.nix {};
+  libtsm' = pkgs.callPackage ../libtsm/default.nix { };
 in
 
 stdenv.mkDerivation (finalAttrs: {
-    pname = "kmscon";
-    version = "9.2.1";
+  pname = "kmscon";
+  version = "9.2.1";
 
-    src = fetchFromGitHub {
-        owner = "kmscon";
-        repo = "kmscon";
-        tag = "v${finalAttrs.version}";
-        hash = "sha256-MuDqZDbZOrq4n/LxEupbBPIL1747iiHD6kM0SeX2Vzc=";
-    };
+  src = fetchFromGitHub {
+    owner = "kmscon";
+    repo = "kmscon";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-MuDqZDbZOrq4n/LxEupbBPIL1747iiHD6kM0SeX2Vzc=";
+  };
 
-    patches = [
-        ./sandbox.patch # Generate system units where they should be (nix store) instead of /etc/systemd/system
-    ];
+  patches = [
+    ./sandbox.patch # Generate system units where they should be (nix store) instead of /etc/systemd/system
+  ];
 
-    strictDeps = true;
+  strictDeps = true;
 
-    depsBuildBuild = [
-        buildPackages.stdenv.cc
-    ];
+  depsBuildBuild = [
+    buildPackages.stdenv.cc
+  ];
 
-    buildInputs = [
-        libGLU
-        libGL
-        libdrm
-        libtsm'
-        libxkbcommon
-        pango
-        pixman
-        systemdLibs
-        libgbm
-        check
-    ];
+  buildInputs = [
+    libGLU
+    libGL
+    libdrm
+    libtsm'
+    libxkbcommon
+    pango
+    pixman
+    systemdLibs
+    libgbm
+    check
+  ];
 
-    nativeBuildInputs = [
-        meson
-        ninja
-        docbook_xsl
-        pkg-config
-        libxslt # xsltproc
-    ];
+  nativeBuildInputs = [
+    meson
+    ninja
+    docbook_xsl
+    pkg-config
+    libxslt # xsltproc
+  ];
 
-    env.NIX_CFLAGS_COMPILE =
-        lib.optionalString stdenv.cc.isGNU "-O "
-        + "-Wno-error=maybe-uninitialized -Wno-error=unused-result -Wno-error=implicit-function-declaration";
+  env.NIX_CFLAGS_COMPILE =
+    lib.optionalString stdenv.cc.isGNU "-O "
+    + "-Wno-error=maybe-uninitialized -Wno-error=unused-result -Wno-error=implicit-function-declaration";
 
-    meta = {
-        description = "KMS/DRM based System Console";
-        mainProgram = "kmscon";
-        homepage = "https://www.freedesktop.org/wiki/Software/kmscon/";
-        license = lib.licenses.mit;
-        maintainers = [ ];
-        platforms = lib.platforms.linux;
-    };
+  meta = {
+    description = "KMS/DRM based System Console";
+    mainProgram = "kmscon";
+    homepage = "https://www.freedesktop.org/wiki/Software/kmscon/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
 })
--- packages/libtsm/default.nix
+++ packages/libtsm/default.nix
@@ -1,41 +1,41 @@
-{
-    lib,
-    stdenv,
-    fetchFromGitHub,
-    meson,
-    ninja,
-    pkg-config,
-    libxkbcommon,
-    check,
+{ lib
+, stdenv
+, fetchFromGitHub
+, meson
+, ninja
+, pkg-config
+, libxkbcommon
+, check
+,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
-    pname = "libtsm";
-    version ="4.4.0";
+  pname = "libtsm";
+  version = "4.4.0";
 
-    src = fetchFromGitHub {
-        owner = "kmscon";
-        repo = "libtsm";
-        tag = "v${finalAttrs.version}";
-        hash = "sha256-xrZNVx9Ir9MCyXiV/cWLdjL2R9KNARY0sFpD5jUlP20=";
-    };
+  src = fetchFromGitHub {
+    owner = "kmscon";
+    repo = "libtsm";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-xrZNVx9Ir9MCyXiV/cWLdjL2R9KNARY0sFpD5jUlP20=";
+  };
 
-    buildInputs = [
-        check
-        libxkbcommon
-    ];
+  buildInputs = [
+    check
+    libxkbcommon
+  ];
 
-    nativeBuildInputs = [
-        meson
-        ninja
-        pkg-config
-    ];
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
 
-    enableParallelBuilding = true;
+  enableParallelBuilding = true;
 
-    meta = {
-        description = "Terminal state machine library";
-        license = lib.licenses.mit;
-        platforms = lib.platforms.linux;
-    };
+  meta = {
+    description = "Terminal state machine library";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+  };
 })
