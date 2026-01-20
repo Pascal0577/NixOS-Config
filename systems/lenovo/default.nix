a83708157def60aed2e7496547dd027a74c43a7a
Pascal0577 <pascalthederg@gmail.com>
2026-01-19 18:26:46 -0600

    using niri stable

--- systems/lenovo/default.nix
+++ systems/lenovo/default.nix
@@ -1,9 +1,9 @@
-{ username, pkgs, ... }:
+{ username, pkgs, lib, ... }:
 {
     imports = [
         ./hardware-configuration.nix
         ../../modules/desktop/gnome
     ];
     home-manager.users.${username}.stylix.targets.gtk.enable = true;
-    programs.niri.package = pkgs.niri-stable;
+    programs.niri.package = lib.mkForce pkgs.niri-stable;
 }
