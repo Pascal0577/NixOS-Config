1553d884a0af610bd9120248d29d9c90c212c483
Pascal0577 <pascalthederg@gmail.com>
2026-01-21 16:39:46 -0600

    minor code cleanup

--- systems/acer/default.nix
+++ systems/acer/default.nix
@@ -1,33 +1,33 @@
 { pkgs, ... }:
+
 {
-    # Declare mathematica module here. I have the sources for it on only one machine
     imports = [
         ./hardware-configuration.nix
         ../../modules/desktop/niri
     ];
 
     services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
-    hardware.nvidia = {
-        open = true;
-        modesetting.enable = true;
-        nvidiaSettings = false;
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
-        prime = {
-            sync.enable = true;
-            intelBusId = "PCI:0:2:0";
-            nvidiaBusId = "PCI:1:0:0";
+    hardware = {
+        nvidia = {
+            open = true;
+            modesetting.enable = true;
+            nvidiaSettings = false;
+            powerManagement.enable = false;
+            powerManagement.finegrained = false;
+            prime = {
+                sync.enable = true;
+                intelBusId = "PCI:0:2:0";
+                nvidiaBusId = "PCI:1:0:0";
+            };
         };
+
+        graphics.extraPackages = with pkgs; [
+            intel-media-driver
+        ];
     };
 
-    hardware.graphics.extraPackages = with pkgs; [
-        intel-media-driver 
-    ];
-
     boot = {
-        initrd = {
-            kernelModules = [ "i915" ];
-        };
+        initrd.kernelModules = [ "i915" ];
 
         kernelParams = [
             "i915.enable_guc=3"
