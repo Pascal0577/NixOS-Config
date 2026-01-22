3055088985a36da211d59853b48625aa4e6da29d
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-01-22 14:48:13 -0600

    added help message

--- assets/dim-screen-smooth.sh
+++ assets/dim-screen-smooth.sh
@@ -2,6 +2,22 @@
 
 set -eu
 
+print_usage() {
+    cat <<- EOF
+Usage: $(basename "$0") [options] set [value]
+
+Options:
+  --steps           number of steps to take to reach target brightness. Higher values seem smoother. Defaults to 20
+  --time            how long it takes to reach target brightness in seconds. Defaults to 1.
+  --no-save         don't save brightness before dimming screen. Prevents 'brightnessctl -r' from restoring brightness
+  -h, --help        print this help message
+
+Values:
+  specific value    Example: 50
+  percentage        Example: 50%
+	EOF
+}
+
 parse_input() {
     while [ $# -gt 0 ]; do
         case "$1" in
@@ -17,18 +33,24 @@ parse_input() {
             --no-save)
                 NOSAVE=true
                 shift ;;
+            -h|--help)
+                print_usage
+                exit 0 ;;
             *)
                 echo "Invalid input: $1" >&2
+                print_usage >&2
                 exit 1
         esac
     done
 }
 
 set_values() {
+    # Set default values if not set
     STEPS="${STEPS:-20}"
     TIME="${TIME:-1}"
     NOSAVE="${NOSAVE:-false}"
 
+    # Handle the case where input is passed as '30%'
     if [ "${INPUT%'%'}" != "$INPUT" ]; then
         PERCENT=true
         INPUT="${INPUT%'%'}"
@@ -66,11 +88,11 @@ checks() {
     if [ -z "$INPUT" ]; then
         echo "No input defined!" >&2
         exit 1
-    fi
-
-    if ! command -v brightnessctl >/dev/null; then
+    elif ! command -v brightnessctl >/dev/null; then
         echo "brightnessctl not installed. Please make sure it is in your PATH" >&2
         exit 1
+    elif ! command -v bc >/dev/null; then
+        echo "bc not installed. Please make sure it is in your PATH" >&2
     fi
 }
 
