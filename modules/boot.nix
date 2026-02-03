ed66f2ff075ce5b15e507e8c536ebf46168b8d5d
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-03 12:05:30 -0600

    restructured gaming config

--- modules/boot.nix
+++ modules/boot.nix
@@ -5,6 +5,14 @@
     boot = {
         kernelPackages = pkgs.linuxPackages_latest;
         bootspec.enable = true;
+
+        kernel.sysctl = {
+            "kernel.sched_cfs_bandwidth_slice_us" = 3000;
+            "net.ipv4.tcp_fin_timeout" = 5;
+            "kernel.split_lock_mitigate" = 0;
+            "vm.max_map_count" = 2147483642;
+        };
+
         plymouth = {
             enable = true;
             theme = lib.mkDefault "bgrt";
--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,35 +1,20 @@
-{ pkgs, inputs, ... }:
+{ pkgs, username, ... }:
 
 {
     environment.systemPackages = with pkgs; [
-        mangohud
         heroic
-        unzip
-        cabextract
         prismlauncher
     ];
 
-    imports = [
-        inputs.nix-gaming.nixosModules.platformOptimizations
-        inputs.nix-gaming.nixosModules.wine
-    ];
+    programs.gamemode = {
+        enable = true;
+        renice = 10;
+    };
 
-    programs = {
-        gamemode.enable = true;
-        wine = {
+    home-manager.users.${username} = {
+        programs.mangohud = {
             enable = true;
-            package = pkgs.wine-staging;
-            binfmt = true;
-            ntsync = true;
-        };
-
-        steam = {
-            enable = false;
-            remotePlay.openFirewall = true;
-            dedicatedServer.openFirewall = true;
-            localNetworkGameTransfers.openFirewall = true;
-            extraCompatPackages =  [ pkgs.proton-ge-bin ];
-            platformOptimizations.enable = true;
+            settings.preset = 3;
         };
     };
 }
