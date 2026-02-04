dbbbf80e5eba07d782bd57ae947f0876391921d4
Pascal0577 <pascalthederg@gmail.com>
2026-02-04 12:43:29 -0600

    moved nvidia-specific obs stuff to acer system

--- modules/applications/obs-studio.nix
+++ modules/applications/obs-studio.nix
@@ -8,22 +8,7 @@
             obs-backgroundremoval
             obs-pipewire-audio-capture
             obs-vaapi
-            obs-gstreamer
             obs-vkcapture
         ];
-
-        package = (
-            pkgs.obs-studio.override {
-                cudaSupport = true;
-                # If we're compiling from source we might as well apply optimizations
-                stdenv = pkgs.stdenvAdapters.withCFlags [
-                    "-O3"
-                    "-march=native"
-                    "-mtune=native"
-                    "-fomit-frame-pointer"
-                    "-flto"
-                ] pkgs.stdenv;
-            }
-        );
     };
 }
