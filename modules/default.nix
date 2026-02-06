798909e27f9e2eafb4c39e60c74106d1fe1241e6
Pascal0577 <pascalthederg@gmail.com>
2026-02-05 20:07:14 -0600

    fixed systemd timeout

--- modules/default.nix
+++ modules/default.nix
@@ -14,8 +14,12 @@
     services.openssh.enable = true;
 
     systemd = {
+        # If a service has tried to stop for longer than 10s something has gone wrong
         user.extraConfig = "DefaultTimeoutStopSec=10s";
-        extraConfig = "DefaultTimeoutStopSec=10s";
+        settings.Manager = {
+            DefaultTimeoutStopSec = "10s";
+        };
+
         services = {
             "serial-getty@".enable = false;
             NetworkManager-wait-online.enable = false;
