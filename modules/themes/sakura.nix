ab4469d7ecac600e7a409d8bff7becf87d06543c
Pascal0577 <pascalthederg@gmail.com>
2026-02-06 13:46:57 -0600

    added sakura theme

---
+++ modules/themes/sakura.nix
@@ -0,0 +1,87 @@
+{ pkgs, inputs, username, lib, ... }:
+
+{
+    imports = [
+        inputs.stylix.nixosModules.stylix
+    ];
+
+    fonts = {
+        packages = with pkgs; [
+            ubuntu-sans
+            ubuntu-sans-mono
+            noto-fonts-cjk-sans
+        ];
+
+        fontconfig = {
+            enable = true;
+            useEmbeddedBitmaps = true;
+        };
+    };
+
+    stylix = {
+        enable = true;
+        autoEnable = true;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/sakura.yaml";
+        image = ../../assets/flowers.png;
+
+        fonts = {
+            serif = {
+                package = pkgs.ubuntu-sans;
+                name = "Ubuntu Sans";
+            };
+
+            sansSerif = {
+                package = pkgs.ubuntu-sans;
+                name = "Ubuntu Sans";
+            };
+
+            monospace = {
+                package = pkgs.nerd-fonts.jetbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+
+            emoji = {
+                package = pkgs.noto-fonts-color-emoji;
+                name = "Noto Color Emoji";
+            };
+        };
+
+        cursor = {
+            package = lib.mkDefault pkgs.bibata-cursors;
+            name = "Bibata-Modern-Ice";
+            size = 24;
+        };
+
+        icons = {
+            enable = true;
+            package = pkgs.papirus-icon-theme;
+            dark = "Papirus-Dark";
+            light = "Papirus-Light";
+        };
+
+        targets = {
+            plymouth.enable = false;
+            qt.enable = false;
+        };
+    };
+
+    home-manager.users.${username} = {
+        stylix.targets = {
+            zen-browser = {
+                enable = true;
+                profileNames = [ "pascal" ];
+            };
+
+            nixvim = {
+                transparentBackground.numberLine = true;
+                transparentBackground.signColumn = true;
+            };
+        };
+
+        dconf.settings."org/gnome/desktop/interface" = {
+            color-scheme = lib.mkForce "prefer-dark";
+            font-antialiasing = lib.mkDefault "standard";
+            font-hinting = lib.mkDefault "full";
+        };
+    };
+}
