617ed7c3940d477552dcc89eab72e4e4d32381f5
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-05 23:02:41 -0600

    fixed launcher configuration

--- modules/applications/launcher/vicinae.nix
+++ modules/applications/launcher/vicinae.nix
@@ -1,6 +1,7 @@
 { inputs, pkgs, username, ... }:
 
 {
+    launcherCommand = "vicinae vicinae://toggle";
     nix.settings = {
         extra-substituters = [ "https://vicinae.cachix.org" ];
         extra-trusted-public-keys = [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
