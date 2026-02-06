6c1d1a3f227a36de0d99acfc734d1ba1abb4b326
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-05 20:03:13 -0600

    services restructure

--- modules/default.nix
+++ modules/default.nix
@@ -5,11 +5,20 @@
         ./locale-time.nix
         ./networking.nix
         ./power-management.nix
-        ./services
         ./users.nix
     ];
 
     security.rtkit.enable = true;
     nixpkgs.config.allowUnfree = true;
     system.stateVersion = "26.05";
+    services.openssh.enable = true;
+
+    systemd = {
+        user.extraConfig = "DefaultTimeoutStopSec=10s";
+        extraConfig = "DefaultTimeoutStopSec=10s";
+        services = {
+            "serial-getty@".enable = false;
+            NetworkManager-wait-online.enable = false;
+        };
+    };
 }
