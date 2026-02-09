3db5856fde40daeae0d48deca2b44df3e58ff533
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-09 16:08:55 -0600

    added oxwm launcher configuration

--- modules/applications/oxwm/default.nix
+++ modules/applications/oxwm/default.nix
@@ -10,19 +10,25 @@
     };
 
     home-manager.users.${username} = {
-        home.file.".config/oxwm/colors.lua".text = ''
-            colors = {
-                fg = "#${config.lib.stylix.colors.base05-hex}",
-                bg = "#${config.lib.stylix.colors.base00-hex}",
-                red = "#${config.lib.stylix.colors.base08-hex}",
-                orange = "#${config.lib.stylix.colors.base09-hex}",
-                yellow = "#${config.lib.stylix.colors.base0A-hex}",
-                green = "#${config.lib.stylix.colors.base0B-hex}",
-                cyan = "#${config.lib.stylix.colors.base0C-hex}",
-                blue = "#${config.lib.stylix.colors.base0D-hex}",
-                purple = "#${config.lib.stylix.colors.base0E-hex}",
-                brown = "#${config.lib.stylix.colors.base0F-hex}",
-            }
-        '';
+        home.file = {
+            ".config/oxwm/colors.lua".text = ''
+                colors = {
+                    fg = "#${config.lib.stylix.colors.base05-hex}",
+                    bg = "#${config.lib.stylix.colors.base00-hex}",
+                    red = "#${config.lib.stylix.colors.base08-hex}",
+                    orange = "#${config.lib.stylix.colors.base09-hex}",
+                    yellow = "#${config.lib.stylix.colors.base0A-hex}",
+                    green = "#${config.lib.stylix.colors.base0B-hex}",
+                    cyan = "#${config.lib.stylix.colors.base0C-hex}",
+                    blue = "#${config.lib.stylix.colors.base0D-hex}",
+                    purple = "#${config.lib.stylix.colors.base0E-hex}",
+                    brown = "#${config.lib.stylix.colors.base0F-hex}",
+                }
+            '';
+
+            ".config/oxwm/launcher.lua".text = ''
+                oxwm.key.bind({ "Mod4" }, "D", oxwm.spawn({ "sh", "-c", "${config.launcherCommand}" }))
+            '';
+        };
     };
 }
