283759ec31017fb80708ca3a4955beb3c1a5a2ca
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-09 16:13:50 -0600

    add modular terminal configuration

---
+++ modules/applications/terminal/default.nix
@@ -0,0 +1,19 @@
+{ lib, ... }:
+
+{
+    options = {
+        terminalOpenWindow = lib.mkOption {
+            type = lib.types.str;
+            default = "ghostty +new-window";
+            description = "Command used to open a new terminal window";
+        };
+
+        terminalRunCommand = lib.mkOption {
+            type = lib.types.str;
+            default = "ghostty -e";
+            description = "Command used to run a command in a new terminal window";
+        };
+    };
+
+    imports = [ ./ghostty.nix ];
+}
