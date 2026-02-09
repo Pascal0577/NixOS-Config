484ed8be5773eaa1fd914b9b1063a2cd92d803d7
Pascal0577 <pascalthederg@gmail.com>
2026-02-09 16:08:55 -0600

    added oxwm launcher configuration

--- modules/applications/launcher/dmenu.nix
+++ modules/applications/launcher/dmenu.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
-
+let
+    hue = config.lib.stylix.colors;
+in
 {
-    launcherCommand = "dmenu_run -nb #${config.lib.stylix.colors.base00-hex}";
+    launcherCommand = "dmenu_run -nb #${hue.base00-hex} -nf #${hue.base05-hex} -sb #${hue.base02-hex} -sf #${hue.base04-hex}";
     environment.systemPackages = [ pkgs.dmenu ];
 }
