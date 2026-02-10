cbb8b971382591fe7de8cb4f37242e72f035ed4a
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-10 11:18:24 -0600

    alacritty on lenovo

--- systems/acer/default.nix
+++ systems/acer/default.nix
@@ -4,6 +4,7 @@
     imports = [
         ./hardware-configuration.nix
         ../../modules
+        ../../modules/applications/terminal/ghostty.nix
         ../../modules/applications/heroic.nix
         ../../modules/themes/everforest.nix
     ];
--- systems/lenovo/default.nix
+++ systems/lenovo/default.nix
@@ -4,6 +4,7 @@
     imports = [
         ./hardware-configuration.nix
         ../../modules
+        ../../modules/applications/terminal/alacritty.nix
         ../../modules/themes/everforest.nix
     ];
 
