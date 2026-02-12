96c89c251802ea68aeab8110066976c1b79f6f00
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-12 11:36:06 -0600

    alacritty tweaks

--- modules/applications/terminal/alacritty.nix
+++ modules/applications/terminal/alacritty.nix
@@ -3,6 +3,15 @@
 {
     terminalOpenWindow = "alacritty";
     home-manager.users.${username} = {
-        programs.alacritty.enable = true;
+        programs.alacritty = {
+            enable = true;
+            settings.cursor = {
+                blink_timeout = 0;
+                style = {
+                    shape = "Beam";
+                    blinking = "On";
+                };
+            };
+        };
     };
 }
