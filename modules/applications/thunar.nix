98f6bf20a96e179607909ec674b1afb30fc9dde9
Pascal0577 <pascalthederg@gmail.com>
2026-02-12 15:26:18 -0600

    modules/applications/thunar: added plugins, thumbnailer

--- modules/applications/thunar.nix
+++ modules/applications/thunar.nix
@@ -1,9 +1,13 @@
 { pkgs, ... }:
 
 {
+    services.gvfs.enable = true;
+    services.tumbler.enable = true;
+    programs.xfconf.enable = true;
+
     programs.thunar = {
         enable = true;
-        plugins = with pkgs [
+        plugins = with pkgs; [
             thunar-archive-plugin
             thunar-volman
         ];
