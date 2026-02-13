ec8fffe2f4b96702c6c630fbe996bd5cbc40c070
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-13 16:10:12 -0600

    applications/kmscon: added conditional logic

--- modules/applications/kmscon.nix
+++ modules/applications/kmscon.nix
@@ -1,12 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 
 {
-    services.kmscon = {
-        enable = true;
-        hwRender = true;
-        fonts = [{
-            name = "JetBrainsMono Nerd Font";
-            package = pkgs.nerd-fonts.jetbrains-mono;
-        }];
+    options.applications.kmscon.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether to enable my kmscon module";
+    };
+
+    config = lib.mkIf config.applications.kmscon.enable {
+        services.kmscon = {
+            enable = true;
+            hwRender = true;
+            fonts = [{
+                name = "JetBrainsMono Nerd Font";
+                package = pkgs.nerd-fonts.jetbrains-mono;
+            }];
+        };
     };
 }
