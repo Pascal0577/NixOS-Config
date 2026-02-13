9772758641ef203aad0521a75361ed3279d4f8b0
Pascal0577 <pascalthederg@gmail.com>
2026-02-13 16:11:06 -0600

    applications/pipewire: added conditional logic

--- modules/applications/pipewire.nix
+++ modules/applications/pipewire.nix
@@ -1,9 +1,19 @@
+{ config, lib, ... }:
+
 {
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
+    options.applications.pipewire.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether to enable my pipewire module";
+    };
+
+    config = lib.mkIf config.applications.pipewire.enable {
+        services.pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+            jack.enable = true;
+        };
     };
 }
