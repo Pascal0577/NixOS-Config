ae3e600c2b0f3df8dee8755f6310199b0e5ca487
Pascal0577 <pascalthederg@gmail.com>
2026-02-13 16:11:26 -0600

    applications/virt-manager: added conditional logic

--- modules/applications/virt-manager.nix
+++ modules/applications/virt-manager.nix
@@ -1,47 +1,55 @@
-{ pkgs, username, ... }:
+{ pkgs, username, config, lib, ... }:
 
 {
-    programs.virt-manager.enable = true;
-
-    users.groups.libvirtd.members = [ "${username}" ];
-
-    environment.systemPackages = [ pkgs.qemu ];
-
-    virtualisation.libvirtd = {
-        enable = true;
-        qemu = {
-            package = pkgs.qemu_kvm;
-            swtpm.enable = true;  # TPM emulation for Windows 11
-        };
+    options.applications.virt-manager.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether to enable my virt-manager module";
     };
 
-    virtualisation.spiceUSBRedirection.enable = true;
+    config = lib.mkIf config.applications.virt-manager.enable {
+        programs.virt-manager.enable = true;
 
-    users.users.${username}.extraGroups = [ "libvirtd" ];
+        users.groups.libvirtd.members = [ "${username}" ];
 
-    # boot = {
-    #     blacklistedKernelModules = [ "nouveau" "nvidia" ];
+        environment.systemPackages = [ pkgs.qemu ];
 
-    #     initrd.kernelModules = [
-    #         "vfio_pci"
-    #         "vfio"
-    #         "vfio_iommu_type1"
-    #     ];            
-
-    #     kernelParams = [
-    #         "intel_iommu=on"
-    #         "iommu=pt"
-    #     ];
-
-    #     extraModprobeConfig = ''
-    #         options vfio-pci ids=10de:28e1,10de:22be
-    #     '';
-    # };
-
-    home-manager.users.${username}.dconf.settings = {
-        "org/virt-manager/virt-manager/connections" = {
-            autoconnect = [ "qemu:///system" ];
-            uris = [ "qemu:///system" ];
+        virtualisation.libvirtd = {
+            enable = true;
+            qemu = {
+                package = pkgs.qemu_kvm;
+                swtpm.enable = true;  # TPM emulation for Windows 11
+            };
         };
-   };
+
+        virtualisation.spiceUSBRedirection.enable = true;
+
+        users.users.${username}.extraGroups = [ "libvirtd" ];
+
+        # boot = {
+        #     blacklistedKernelModules = [ "nouveau" "nvidia" ];
+
+        #     initrd.kernelModules = [
+        #         "vfio_pci"
+        #         "vfio"
+        #         "vfio_iommu_type1"
+        #     ];            
+
+        #     kernelParams = [
+        #         "intel_iommu=on"
+        #         "iommu=pt"
+        #     ];
+
+        #     extraModprobeConfig = ''
+        #         options vfio-pci ids=10de:28e1,10de:22be
+        #     '';
+        # };
+
+        home-manager.users.${username}.dconf.settings = {
+            "org/virt-manager/virt-manager/connections" = {
+                autoconnect = [ "qemu:///system" ];
+                uris = [ "qemu:///system" ];
+            };
+       };
+    };
 }
