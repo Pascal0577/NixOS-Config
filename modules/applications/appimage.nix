eb2bf99b950e3a80f1a18ec6a3937933a92a561c
Pascal0577 <pascalthederg@gmail.com>
2026-02-13 15:30:54 -0600

    applications/appimage: add conditional logic

--- modules/applications/appimage.nix
+++ modules/applications/appimage.nix
@@ -1,15 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 
 {
-    programs.appimage = {
-        enable = true;
-        binfmt = true;
-        package = pkgs.appimage-run.override { extraPkgs = pkgs: with pkgs; [
-            libxcrypt
-            libxcrypt-legacy
-            libffi
-            libyaml
-            icu
-        ];};
+    options.applications.appimage.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        descripton = "Whether to enable my AppImage module";
+    };
+
+    config = lib.mkIf config.applications.appimage.enable {
+        programs.appimage = {
+            enable = true;
+            binfmt = true;
+            package = pkgs.appimage-run.override { extraPkgs = pkgs: with pkgs; [
+                libxcrypt
+                libxcrypt-legacy
+                libffi
+                libyaml
+                icu
+            ];};
+        };
     };
 }
