6af9d3c10a84d87c32a098a714007bf6267bb5a0
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-13 23:18:47 -0600

    init: onlyoffice.nix

---
+++ modules/applications/onlyoffice.nix
@@ -0,0 +1,22 @@
+{ config, lib, username, ... }:
+
+{
+    options.applications.onlyoffice.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether to enable my OnlyOffice module";
+    };
+
+    config = lib.mkIf config.applications.onlyoffice.enable {
+        home-manager.users.${username}.programs.onlyoffice = {
+            enable = true;
+            settings = ''
+                UITheme = "theme-night";
+                openPath=/home/pascal/Documents
+                savePath=/home/pascal/Documents
+                titlebar = "custom";
+                editorWindowMode=false
+            '';
+        };
+    };
+}
