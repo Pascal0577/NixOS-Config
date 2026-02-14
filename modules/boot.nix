70ed4c3ed0678d1ce6b045187c7c14ef1bd7044d
Pascal0577 <pascalthederg@gmail.com>
2026-02-13 22:38:49 -0600

    modules/boot: fixed missing bootloader with non-secure boot setup

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,58 +1,87 @@
-{ pkgs, inputs, lib, ... }:
+{ pkgs, inputs, config, lib, ... }:
 
 {
-    imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
-    boot = {
-        kernelPackages = pkgs.linuxPackages_latest;
-        bootspec.enable = true;
-
-        kernel.sysctl = {
-            "kernel.sched_cfs_bandwidth_slice_us" = 3000;
-            "net.ipv4.tcp_fin_timeout" = 5;
-            "kernel.split_lock_mitigate" = 0;
-            "vm.max_map_count" = 2147483642;
+    options.boot = {
+        enableSecureBoot = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Whether to enable secure boot";
         };
 
-        plymouth = {
-            enable = true;
-            theme = lib.mkDefault "bgrt";
-        };
-
-        lanzaboote = {
-            enable = true;
-            pkiBundle = "/var/lib/sbctl/";
-        };
-
-        # Enable "Silent boot"
-        consoleLogLevel = 4;
-        initrd = {
-            verbose = false;
-            systemd.enable = true;
-        };
-
-        kernelParams = [
-            # silent boot
-            "quiet"
-            "splash"
-            "nowatchdog"
-            "boot.shell_on_fail"
-            "udev.log_priority=3"
-            "rd.systemd.show_status=auto"
-            # security stuff
-            "rcupdate.rcu_expedited=1"
-            "page_alloc.shuffle=1"
-            # zswap
-            "zswap.enabled=1"
-            "zswap.compressor=zstd"
-            "zswap.max_pool_percent=50"
-            "zswap.shrinker_enabled=1"
-        ];
-
-        loader = {
-            timeout = 0;
-            efi.canTouchEfiVariables = true;
+        enablePlymouth = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Whether to enable the Plymouth bootsplash screen";
         };
     };
+
+    imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
+
+    config.boot = lib.mkMerge [
+        {
+            kernelPackages = pkgs.linuxPackages_latest;
+            bootspec.enable = true;
+            initrd.systemd.enable = true;
+
+            loader = {
+                efi.canTouchEfiVariables = true;
+                systemd-boot.enable = true;
+            };
+
+            kernel.sysctl = {
+                "kernel.sched_cfs_bandwidth_slice_us" = 3000;
+                "net.ipv4.tcp_fin_timeout" = 5;
+                "kernel.split_lock_mitigate" = 0;
+                "vm.max_map_count" = 2147483642;
+            };
+
+            kernelParams = [
+                # security stuff
+                "rcupdate.rcu_expedited=1"
+                "page_alloc.shuffle=1"
+                # zswap
+                "zswap.enabled=1"
+                "zswap.compressor=zstd"
+                "zswap.max_pool_percent=50"
+                "zswap.shrinker_enabled=1"
+            ];
+        }
+
+        (lib.mkIf config.boot.enablePlymouth {
+            plymouth = {
+                enable = true;
+                theme = lib.mkDefault "bgrt";
+            };
+
+            # Enable "Silent boot"
+            consoleLogLevel = 4;
+            initrd.verbose = false;
+
+            loader = {
+                timeout = 0;
+                systemd-boot.enable = lib.mkForce false;
+            };
+
+            kernelParams = [
+                # silent boot
+                "quiet"
+                "splash"
+                "nowatchdog"
+                "boot.shell_on_fail"
+                "udev.log_priority=3"
+                "rd.systemd.show_status=auto"
+            ];
+        })
+
+        (lib.mkIf config.boot.enableSecureBoot {
+            lanzaboote = {
+                enable = true;
+                pkiBundle = "/var/lib/sbctl/";
+                autoGenerateKeys.enable = true;
+                autoEnrollKeys.enable = true;
+            };
+        })
+    ];
 }
 
 # More work needs to be done if I want to enable secure boot.
