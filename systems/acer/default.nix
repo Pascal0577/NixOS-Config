18124ae091ded6aba2e6c1c253a6cf68c0b9efac
Pascal0577 <pascalthederg@gmail.com>
2026-02-15 18:22:10 -0600

    fixed nvidia issues

--- systems/acer/default.nix
+++ systems/acer/default.nix
@@ -35,15 +35,33 @@
     hardware = {
         graphics.extraPackages = [ pkgs.intel-media-driver ];
         nvidia = {
-            open = false;
+            open = true;
             modesetting.enable = true;
             nvidiaSettings = false;
             powerManagement.enable = false;
             powerManagement.finegrained = false;
+            dynamicBoost.enable = true;
             prime = {
                 sync.enable = true;
                 intelBusId = "PCI:0:2:0";
                 nvidiaBusId = "PCI:1:0:0";
+            };
+            package =
+            let
+                base = config.boot.kernelPackages.nvidiaPackages.latest;
+                cachyos-nvidia-patch = pkgs.fetchpatch {
+                    url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+                    sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+                };
+
+                # Patch the appropriate driver based on config.hardware.nvidia.open
+                driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+            in
+            base
+            // {
+                ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+                    patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+                });
             };
         };
 
