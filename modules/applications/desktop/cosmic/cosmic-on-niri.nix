6df7b396437dfdb9ce2d85f19f0e94ae8198e199
Pascal0577 <pascalthederg@gmail.com>
2026-02-16 17:13:59 -0600

    fixed some cosmic jank

--- modules/applications/desktop/cosmic/cosmic-on-niri.nix
+++ modules/applications/desktop/cosmic/cosmic-on-niri.nix
@@ -7,12 +7,33 @@
         description = "Whether to enable the Niri compositor for the COSMIC session";
     };
 
-    config = lib.mkIf config.desktop.config.cosmicOnNiri.enable {
+    config = lib.mkIf config.desktop.cosmic.cosmicOnNiri.enable {
         environment = {
             systemPackages = [
                 (pkgs.callPackage ../../../../packages/cosmic-ext-niri/default.nix {})
+                pkgs.adw-gtk3
             ];
             pathsToLink = [ "/share/wayland-sessions" ];
+        };
+
+        xdg.portal = {
+            enable = true;
+            extraPortals = with pkgs; [
+                xdg-desktop-portal-cosmic
+                xdg-desktop-portal-gnome
+                xdg-desktop-portal-gtk
+            ];
+            config = {
+                niri = {
+                    default = [ "gnome" "gtk" ];
+                    "org.freedesktop.impl.portal.FileChooser" = [ "gnome" ];
+                    "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+                    "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+                };
+                cosmic = {
+                    default = [ "cosmic" "gtk" ];
+                };
+            };
         };
 
         launcher = {
@@ -22,8 +43,9 @@
 
         home-manager.users.${username} = {
             programs.niri.settings.layout.border = {
-                color = lib.mkForce "#${config.desktop.cosmic.accentColor}";
+                active.color = lib.mkForce "#${config.desktop.cosmic.accentColor}";
             };
+            stylix.targets.gtk.enable = false;
         };
     };
 }
