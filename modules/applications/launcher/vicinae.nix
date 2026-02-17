4b38fea90e25e3e54df388f8f7cc3aa565312a2d
Pascal0577 <pascalthederg@gmail.com>
2026-02-17 12:47:04 -0600

    make all substituters always imported

--- modules/applications/launcher/vicinae.nix
+++ modules/applications/launcher/vicinae.nix
@@ -9,97 +9,101 @@ in
         description = "Whether to enable my vicinae module";
     };
 
-    config = lib.mkIf config.mySystem.applications.launcher.vicinae.enable {
-        mySystem.applications.launcher = {
-            package = inputs.vicinae.packages.${hostSystem}.default;
-            command = "vicinae vicinae://toggle";
-        };
+    config = lib.mkMerge [
+        {
+            nix.settings = {
+                extra-substituters = [ "https://vicinae.cachix.org" ];
+                extra-trusted-public-keys = [ 
+                    "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+                ];
+            };
+        }
 
-        nix.settings = {
-            extra-substituters = [ "https://vicinae.cachix.org" ];
-            extra-trusted-public-keys = [ 
-                "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-            ];
-        };
-
-        home-manager.users.${username} = {
-            imports = [
-                inputs.vicinae.homeManagerModules.default
-            ];
-
-            services.vicinae = {
-                enable = true;
+        (lib.mkIf config.mySystem.applications.launcher.vicinae.enable {
+            mySystem.applications.launcher = {
                 package = inputs.vicinae.packages.${hostSystem}.default;
-                systemd = {
+                command = "vicinae vicinae://toggle";
+            };
+
+            home-manager.users.${username} = {
+                imports = [
+                    inputs.vicinae.homeManagerModules.default
+                ];
+
+                services.vicinae = {
                     enable = true;
-                    autoStart = true;
-                    environment = { USE_LAYER_SHELL = 1; };
-                };
-                settings = {
-                    favicon_service = "twenty";
-                    pop_to_root_on_close = true;
-                    search_files_in_root = true;
-
-                    font = {
-                        normal = {
-                            size = 12;
-                            normal = "Ubuntu Sans";
-                        };
+                    package = inputs.vicinae.packages.${hostSystem}.default;
+                    systemd = {
+                        enable = true;
+                        autoStart = true;
+                        environment = { USE_LAYER_SHELL = 1; };
                     };
+                    settings = {
+                        favicon_service = "twenty";
+                        pop_to_root_on_close = true;
+                        search_files_in_root = true;
 
-                    theme = {
-                        light = {
-                            name = "stylix";
-                            icon_theme = "Papirus-Light";
+                        font = {
+                            normal = {
+                                size = 12;
+                                normal = "Ubuntu Sans";
+                            };
                         };
-                        dark = {
-                            name = "stylix";
-                            icon_theme = "Papirus-Dark";
+
+                        theme = {
+                            light = {
+                                name = "stylix";
+                                icon_theme = "Papirus-Light";
+                            };
+                            dark = {
+                                name = "stylix";
+                                icon_theme = "Papirus-Dark";
+                            };
                         };
-                    };
 
-                    launcher_window = {
-                        csd = true;
-                        opacity = 1.0;
-                    };
-
-                    extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
-                    ];
-
-                    favorites = [
-                        "applications:zen-beta"
-                        "applications:vesktop"
-                        "clipboard:history"
-                    ];
-
-                    providers = {
-                        clipboard.preferences.encryption = true;
-                        core.entrypoints = {
-                            about.enabled = false;
-                            documentation.enabled = false;
-                            keybind-settings.enabled = false;
-                            manage-fallback.enabled = false;
-                            oauth-token-store.enabled = false;
-                            open-config-file.enabled = false;
-                            open-default-config.enabled = false;
-                            report-bug.enabled = false;
-                            sponsor.enabled = false;
+                        launcher_window = {
+                            csd = true;
+                            opacity = 1.0;
                         };
-                        power.entrypoints = {
-                            lock.enabled = false;
-                            logout.enabled = false;
-                            sleep.enabled = false;
-                            soft-reboot.enabled = false;
-                            suspend.enabled = false;
+
+                        extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
+                        ];
+
+                        favorites = [
+                            "applications:zen-beta"
+                            "applications:vesktop"
+                            "clipboard:history"
+                        ];
+
+                        providers = {
+                            clipboard.preferences.encryption = true;
+                            core.entrypoints = {
+                                about.enabled = false;
+                                documentation.enabled = false;
+                                keybind-settings.enabled = false;
+                                manage-fallback.enabled = false;
+                                oauth-token-store.enabled = false;
+                                open-config-file.enabled = false;
+                                open-default-config.enabled = false;
+                                report-bug.enabled = false;
+                                sponsor.enabled = false;
+                            };
+                            power.entrypoints = {
+                                lock.enabled = false;
+                                logout.enabled = false;
+                                sleep.enabled = false;
+                                soft-reboot.enabled = false;
+                                suspend.enabled = false;
+                            };
+                            developer.enabled = false;
+                            font.enabled = false;
+                            system.enabled = false;
+                            theme.enabled = false;
+                            wm.enabled = false;
                         };
-                        developer.enabled = false;
-                        font.enabled = false;
-                        system.enabled = false;
-                        theme.enabled = false;
-                        wm.enabled = false;
                     };
                 };
             };
-        };
-    };
+        })
+    ];
 }
