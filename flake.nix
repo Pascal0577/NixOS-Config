fccf1ebc5ead6ea3a12327ce221e7802dab8b0f3
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-19 11:51:42 -0600

    update flake.lock

--- flake.nix
+++ flake.nix
@@ -45,8 +45,7 @@
         hyprland.url = "github:vaxerski/Hyprland/layouts-rethonked";
         mithril-shell.url = "github:andreashgk/mithril-shell";
 
-        #oxwm.url = "github:tonybanters/oxwm";
-        oxwm.url = "path:/home/pascal/src/oxwm";
+        oxwm.url = "github:tonybanters/oxwm";
         oxwm.inputs.nixpkgs.follows = "nixpkgs";
     };
 
