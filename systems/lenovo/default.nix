4e1b4fda58a136fc05fea4454ea026cf7a872544
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-02-21 23:07:25 -0600

    add option for stable niri package

--- systems/lenovo/default.nix
+++ systems/lenovo/default.nix
@@ -8,17 +8,22 @@
     mySystem = {
         applications.terminal.alacritty.enable = true;
         applications.launcher.dmenu.enable = true;
-        desktop.oxwm.enable = true;
-        desktop.cosmic = {
-            enable = false;
-            accentColor = "${config.lib.stylix.colors.base09-hex}";
-            accentRed = "${config.lib.stylix.colors.base09-dec-r}";
-            accentGreen = "${config.lib.stylix.colors.base09-dec-g}";
-            accentBlue = "${config.lib.stylix.colors.base09-dec-b}";
-            cosmicOnNiri.enable = false;
+        desktop = {
+            oxwm.enable = true;
+            niri = {
+                enable = false;
+                stable = true;
+            };
+            cosmic = {
+                enable = false;
+                accentColor = "${config.lib.stylix.colors.base09-hex}";
+                accentRed = "${config.lib.stylix.colors.base09-dec-r}";
+                accentGreen = "${config.lib.stylix.colors.base09-dec-g}";
+                accentBlue = "${config.lib.stylix.colors.base09-dec-b}";
+                cosmicOnNiri.enable = false;
+            };
         };
     };
 
     services.displayManager.ly.enable = true;
-    # programs.niri.package = lib.mkForce pkgs.niri-stable;
 }
