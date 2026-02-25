48a39a58604c75b443ada2bf6f21ff60bc992297
Pascal0577 <pascalthederg@gmail.com>
2026-02-24 18:36:32 -0600

    remove x86-specific sysctl

--- modules/boot.nix
+++ modules/boot.nix
@@ -31,7 +31,6 @@
             kernel.sysctl = {
                 "kernel.sched_cfs_bandwidth_slice_us" = 3000;
                 "net.ipv4.tcp_fin_timeout" = 5;
-                "kernel.split_lock_mitigate" = 0;
                 "vm.max_map_count" = 2147483642;
             };
 
