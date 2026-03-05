547492efaf2d9ddae4ba3af6bdacbe556ef615bf
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-04 22:16:21 -0600

    remove zstd compression from home subvol

--- systems/acer/hardware-configuration.nix
+++ systems/acer/hardware-configuration.nix
@@ -26,7 +26,7 @@
     fileSystems."/home" = {
         device = "/dev/disk/by-uuid/06c0c17e-87c3-4411-8897-52aadfbd5023";
         fsType = "btrfs";
-        options = [ "subvol=@home" "compress=zstd" ];
+        options = [ "subvol=@home" ];
     };
 
     fileSystems."/boot" = {
