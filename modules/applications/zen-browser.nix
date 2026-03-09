3f41053cfa44ee7f884952e481fe20fae007b3c0
Pascal0577 <pascalthederg@gmail.com>
2026-03-09 18:19:38 -0500

    remove xdg migration warning

--- modules/applications/zen-browser.nix
+++ modules/applications/zen-browser.nix
@@ -13,7 +13,6 @@
 
             programs.zen-browser = {
                 enable = true;
-                suppressXdgMigrationWarning = true;
                 policies =
                 let
                     mkLockedAttrs = builtins.mapAttrs (_: value: {
