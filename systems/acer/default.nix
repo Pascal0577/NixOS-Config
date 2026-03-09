c444badd1cbab65b0fe26df1df4886e37acff608
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-09 18:21:42 -0500

    remove nvidia patch

--- systems/acer/default.nix
+++ systems/acer/default.nix
@@ -75,23 +75,6 @@
                 intelBusId = "PCI:0:2:0";
                 nvidiaBusId = "PCI:1:0:0";
             };
-            package =
-            let
-                base = config.boot.kernelPackages.nvidiaPackages.latest;
-                cachyos-nvidia-patch = pkgs.fetchpatch {
-                    url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
-                    sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
-                };
-
-                # Patch the appropriate driver based on config.hardware.nvidia.open
-                driverAttr = if config.hardware.nvidia.open then "open" else "bin";
-            in
-            base
-            // {
-                ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
-                    patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
-                });
-            };
         };
     };
 
