22e6db60f2180815d7652645d2d9f988c68fbd01
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-24 13:40:39 -0500

    update home stateVersion

--- modules/users.nix
+++ modules/users.nix
@@ -25,7 +25,7 @@
             home = {
                 username = username;
                 homeDirectory = "/home/${username}";
-                stateVersion = "25.11";
+                stateVersion = "26.05";
             };
         };
     };
