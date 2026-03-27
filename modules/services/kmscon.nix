f595d2feed846aed2d8fa174ec3214c43639195f
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-27 18:36:42 -0500

    remove harmful restriction on kmsconvt service

--- modules/services/kmscon.nix
+++ modules/services/kmscon.nix
@@ -15,39 +15,26 @@
             }];
         };
 
-        systemd.services."kmscon@".serviceConfig = {
-            NoNewPrivileges = true;
-            ProtectSystem = "strict";
+        systemd.services."kmsconvt@".serviceConfig = {
+            ProtectSystem = "full";
             ProtectControlGroups = true;
-            ProtectHome = true;
             ProtectHostname = true;
             ProtectKernelTunables = true;
             ProtectKernelModules = true;
             ProtectKernelLogs = true;
             ProtectClock = true;
-            PrivateMounts = true;
-            PrivateTmp = true;
-            RestrictSUIDSGID = true;
+            # RestrictSUIDSGID = true;
             RestrictRealtime = true;
-            RestrictAddressFamilies = [ 
-                "AF_UNIX"
-                "AF_NETLINK"
-            ];
-            RestrictNamespaces = true;
             SystemCallErrorNumber = "EPERM";
             SystemCallArchitectures = "native";
             SystemCallFilter = [
                 "~@obsolete"
-                "~@debug"
                 "~@reboot"
                 "~@swap"
                 "~@clock"
                 "~@cpu-emulation"
             ];
             LockPersonality = true;
-            IPAddressDeny = ["0.0.0.0/0" "::/0"];
-            MemoryDenyWriteExecute = true;
-            UMask = 0077;
         };
     };
 }
