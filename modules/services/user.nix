f6d24cc9cb9bce4ed253ea0daf5fde3137e801a3
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-27 18:40:23 -0500

    init: modules/services/user.nix

---
+++ modules/services/user.nix
@@ -0,0 +1,23 @@
+{
+    systemd.services."user@".serviceConfig = {
+        ProtectSystem = "full";
+        ProtectControlGroups = true;
+        ProtectHostname = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        # RestrictSUIDSGID = true;
+        RestrictRealtime = true;
+        SystemCallErrorNumber = "EPERM";
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+            "~@obsolete"
+            "~@reboot"
+            "~@swap"
+            "~@clock"
+            "~@cpu-emulation"
+        ];
+        LockPersonality = true;
+    };
+}
