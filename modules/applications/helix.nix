e15f9cd0eabcedcb166e1061603fb26929071867
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-27 18:12:58 -0500

    default to four spaces in helix config for most used langs

--- modules/applications/helix.nix
+++ modules/applications/helix.nix
@@ -4,29 +4,36 @@
     options.mySystem.applications.helix.enable = lib.mkEnableOption "Helix module";
 
     config = lib.mkIf config.mySystem.applications.helix.enable {
-        home-manager.users.${username} = {
-            programs.helix = {
-                enable = true;
-                extraPackages = with pkgs; [
-                    nixd
-                    nixfmt
-                    zls
-                    bash-language-server
-                ];
-                settings = {
-                    editor = {
-                        line-number = "relative";
-                        indent-guides = {
-                            render = true;
-                            character = "┊";
-                        };
-                        cursor-shape = {
-                            normal = "block";
-                            insert = "bar";
-                            select = "underline";
-                        };
+        home-manager.users.${username}.programs.helix = {
+            enable = true;
+            extraPackages = with pkgs; [
+                nixd
+                nixfmt
+                zls
+                bash-language-server
+            ];
+            settings = {
+                editor = {
+                    line-number = "relative";
+                    indent-guides = {
+                        render = true;
+                        character = "┊";
+                    };
+                    cursor-shape = {
+                        normal = "block";
+                        insert = "bar";
+                        select = "underline";
                     };
                 };
+            };
+            languages = {
+                langauge = map (l: {
+                    name = "${l}";
+                    indent = {
+                        tab-width = 4;
+                        unit = "    ";
+                    };
+                }) [ "nix" "zig" "bash" ];
             };
         };
     };
