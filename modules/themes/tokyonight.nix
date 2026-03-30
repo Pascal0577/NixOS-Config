35ac3e43c83ad3879785b06df517eb469e0f781c
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-30 12:22:13 -0500

    change niri border color in tokyonight scheme

--- modules/themes/tokyonight.nix
+++ modules/themes/tokyonight.nix
@@ -1,6 +1,8 @@
 { pkgs, username, lib, config, ... }:
 let
     nvim = config.mySystem.applications.neovim;
+    niri = config.mySystem.desktop.niri;
+    hue = config.lib.stylix.colors;
 in
 {
     options.mySystem.theme.tokyonight.enable = lib.mkEnableOption "Tokyo Night theme";
@@ -11,17 +13,23 @@ in
             image = ../../assets/tokyonight_original.png;
         };
 
-        home-manager.users.${username} = {
-            programs.nixvim = lib.mkIf nvim.enable {
-                colorschemes.tokyonight = {
-                    enable = true;
-                    settings.style = "moon";
-                };
+        home-manager.users.${username} = lib.mkMerge [
+            (lib.mkIf nvim.enable {
+                programs.nixvim = {
+                    colorschemes.tokyonight = {
+                        enable = true;
+                        settings.style = "moon";
+                    };
 
-                performance.combinePlugins.standalonePlugins = [
-                    pkgs.vimPlugins.tokyonight-nvim
-                ];
-            };
-        };
+                    performance.combinePlugins.standalonePlugins = [
+                        pkgs.vimPlugins.tokyonight-nvim
+                    ];
+                };
+            })
+
+            (lib.mkIf niri.enable {
+                programs.niri.settings.layout.border.active.color = "#${hue.base08-hex}";
+            })
+        ];
     };
 }
