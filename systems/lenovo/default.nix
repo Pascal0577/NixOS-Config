b03648a76877acbcc59f23ea08951f5bb977cd51
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-03-30 11:48:41 -0500

    lenovo: uninstall neovim

--- systems/lenovo/default.nix
+++ systems/lenovo/default.nix
@@ -4,6 +4,7 @@
         desktop.niri.enable = true;
         applications = {
             helix.enable = true;
+            neovim.enable = false;
             terminal.foot.enable = true;
             launcher.vicinae.enable = true;
             file-manager.yazi.enable = true;
