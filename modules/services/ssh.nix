17a7694195b904ec70733ad2f29c28bec33397d7
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-04-27 18:27:43 -0500

    pls

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -5,6 +5,9 @@
         lib.mkEnableOption "SSH service configuration" // { default = true; };
 
     config = lib.mkIf config.mySystem.applications.ssh.enable {
+        boot.kernelParams = [ "console=ttyS0" ];
+        services.getty.autologinUser = "root";
+
         users.users.${username}.openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcq68JNj92VwwUXhtxLw/yfDStY2dgroWJC3WQIFErx pascal@acer"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/wHEHpwkF1VwCS/MxZf2cvECIeUHdbiLjO1J1jz0LL pascal@lenovo"
@@ -23,41 +26,41 @@
             };
         };
 
-        systemd.services.sshd.serviceConfig = {
-            NoNewPrivileges = true;
-            ProtectSystem = "full";
-            ProtectClock = true; 
-            ProtectHostname = true;
-            ProtectKernelTunables = true;
-            ProtectKernelModules = true;
-            ProtectKernelLogs = true;
-            ProtectControlGroups = true; 
-            ProtectProc = "invisible";
-            PrivateTmp = true;
-            PrivateMounts = true;
-            PrivateDevices = true;
-            RestrictNamespaces = true;
-            RestrictRealtime = true;
-            RestrictSUIDSGID = true;
-            MemoryDenyWriteExecute = true;
-            LockPersonality = true;
-            DevicePolicy = "closed";
-            SystemCallArchitectures = "native";
-            SystemCallFilter = [
-              "~@keyring"
-              "~@swap"
-              "~@clock"         
-              "~@module"
-              "~@obsolete"
-              "~@cpu-emulation"
-              "~@debug"
-            ];
-            CapabilityBoundingSet = [
-                "~CAP_SETUID"
-                "~CAP_SETGID"
-                "~CAP_SETPCAP"
-                "~CAP_SYS_PTRACE"
-            ];
-        };
+        # systemd.services.sshd.serviceConfig = {
+        #     NoNewPrivileges = true;
+        #     ProtectSystem = "full";
+        #     ProtectClock = true; 
+        #     ProtectHostname = true;
+        #     ProtectKernelTunables = true;
+        #     ProtectKernelModules = true;
+        #     ProtectKernelLogs = true;
+        #     ProtectControlGroups = true; 
+        #     ProtectProc = "invisible";
+        #     PrivateTmp = true;
+        #     PrivateMounts = true;
+        #     PrivateDevices = true;
+        #     RestrictNamespaces = true;
+        #     RestrictRealtime = true;
+        #     RestrictSUIDSGID = true;
+        #     MemoryDenyWriteExecute = true;
+        #     LockPersonality = true;
+        #     DevicePolicy = "closed";
+        #     SystemCallArchitectures = "native";
+        #     SystemCallFilter = [
+        #       "~@keyring"
+        #       "~@swap"
+        #       "~@clock"         
+        #       "~@module"
+        #       "~@obsolete"
+        #       "~@cpu-emulation"
+        #       "~@debug"
+        #     ];
+        #     CapabilityBoundingSet = [
+        #         "~CAP_SETUID"
+        #         "~CAP_SETGID"
+        #         "~CAP_SETPCAP"
+        #         "~CAP_SYS_PTRACE"
+        #     ];
+        # };
     };
 }
