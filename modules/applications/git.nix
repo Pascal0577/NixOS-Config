a0a3bce6cdbd67aa41bd35b5105efb4bdd62a312
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-04-27 22:01:04 -0500

    add email and username to git config

--- modules/applications/git.nix
+++ modules/applications/git.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, username, ... }:
 
 {
     options.mySystem.applications.git.enable =
@@ -7,5 +7,10 @@
     config = lib.mkIf config.mySystem.applications.git.enable {
         environment.systemPackages = [ pkgs.gh ];
         programs.git.enable = true;
+
+        home-manager.users.${username}.programs.git.settings = {
+            user.email = "181921662+Pascal0577@users.noreply.github.com";
+            user.name = "Pascal0577";
+        };
     };
 }
