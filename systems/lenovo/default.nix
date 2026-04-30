1b78041ebc3e0589d3a1343bae6a78f4e34fbe76
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-04-30 18:03:11 -0500

    run ZFS on Lenovo laptop

--- systems/lenovo/default.nix
+++ systems/lenovo/default.nix
@@ -1,6 +1,8 @@
 {
     mySystem = {
         theme.everforest.enable = true;
+        impermanence.enable = true;
+        ZFS.enable = true;
         desktop.niri.enable = true;
         applications = {
             helix.enable = true;
@@ -9,6 +11,7 @@
             launcher.fuzzel.enable = true;
             file-manager.yazi.enable = true;
             heroic.enable = false;
+            mathematica.enable = false;
         };
     };
 }
--- systems/lenovo/hardware-configuration.nix
+++ systems/lenovo/hardware-configuration.nix
@@ -1,6 +1,8 @@
-{ config, lib, ... }:
+{ config, lib, username, inputs, ... }:
 
 {
+    imports = [ inputs.disko.nixosModules.disko ];
+
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     hardware.enableRedistributableFirmware = lib.mkDefault true;
@@ -10,27 +12,166 @@
         initrd = {
             availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
             kernelModules = [ "dm-snapshot" ];
-            luks.devices.root = {
-                device = "/dev/disk/by-uuid/e66aa624-e1c3-4f8a-9908-03d3ffedee8d";
-                preLVM = true;
-            };
         };
     };
 
-    fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/7998-BD7F";
-        fsType = "vfat";
-        options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-    fileSystems."/" = {
-        device = "/dev/disk/by-uuid/4eb773d6-e158-40e0-9f34-f629e94fa421";
-        fsType = "xfs";
-    };
-
-    swapDevices = [{
-        device = "/dev/disk/by-uuid/c1d7d20a-7857-4a1d-8987-f3cd81b26a2f";
-    }];
-
+    # ZFS Stuff
     networking.hostId = "4e98920d";
+    services.zfs.autoScrub = {
+        enable = true;
+        interval = "monthly";
+    };
+
+    disko.devices = {
+        disk = {
+            ssd = {
+                type = "disk";
+                device = "/dev/disk/by-id/nvme-KBG40ZMT128G_TOSHIBA_MEMORY_31GPD8WJQXQ2";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        ESP = {
+                            size = "1G";
+                            type = "EF00";
+                            content = {
+                                type = "filesystem";
+                                format = "vfat";
+                                mountpoint = "/boot";
+                                mountOptions = [ "umask=0077" ];
+                            };
+                        };
+                        swap = {
+                            size = "8G";
+                            content = {
+                                type = "swap";
+                                randomEncryption = true;
+                            };
+                        };
+                        root = {
+                            size = "100%";
+                            content = {
+                                type = "luks";
+                                name = "cryptssd";
+                                content = {
+                                    type = "zfs";
+                                    pool = "ssd";
+                                };
+                            };
+                        };
+                    };
+                };
+            };
+
+            hdd = {
+                type = "disk";
+                device = "/dev/disk/by-id/wwn-0x5000c500d69066f4";
+                content = {
+                    type = "gpt";
+                    partitions.storage = {
+                        size = "100%";
+                        content = {
+                            type = "luks";
+                            name = "crypthdd";
+                            content = {
+                                type = "zfs";
+                                pool = "hdd";
+                            };
+                        };
+                    };
+                };
+            };
+        };
+
+        zpool = {
+            ssd = {
+                type = "zpool";
+                rootFsOptions = {
+                    acltype = "posixacl";
+                    atime = "off";
+                    compression = "lz4";
+                    mountpoint = "none";
+                    xattr = "sa";
+                    dnodesize = "auto";
+                };
+                options = {
+                    ashift = "12";
+                    autotrim = "on";
+                };
+
+                datasets = {
+                    "local" = {
+                        type = "zfs_fs";
+                        options.mountpoint = "none";
+                    };
+                    "local/home" = {
+                        type = "zfs_fs";
+                        mountpoint = "/home";
+                        options."com.sun:auto-snapshot" = "true";
+                    };
+                    "local/vms" = {
+                        type = "zfs_fs";
+                        mountpoint = "/home/${username}/Machines";
+                        options = {
+                            # optimize for big files
+                            recordsize = "1M";
+                            primarycache = "metadata";
+                            logbias = "throughput";
+                            secondarycache = "none";
+                            sync = "disabled";
+                            redundant_metadata = "most";
+                            "com.sun:auto-snapshot" = "false";
+                        };
+                    };
+                    "local/nix" = {
+                        type = "zfs_fs";
+                        mountpoint = "/nix";
+                        options = {
+                            # Nix store has lots of small files
+                            recordsize = "64K";
+                            "com.sun:auto-snapshot" = "false";
+                        };
+                    };
+                    "local/persist" = {
+                        type = "zfs_fs";
+                        mountpoint = "/nix/persist";
+                        options."com.sun:auto-snapshot" = "false";
+                    };
+                    "local/root" = {
+                        type = "zfs_fs";
+                        mountpoint = "/";
+                        options."com.sun:auto-snapshot" = "false";
+                        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^ssd/local/root@blank$' || zfs snapshot ssd/local/root@blank";
+                    };
+                };
+            };
+
+            hdd = {
+                type = "zpool";
+                rootFsOptions = {
+                    acltype = "posixacl";
+                    atime = "off";
+                    compression = "zstd";
+                    mountpoint = "none";
+                    xattr = "sa";
+                    dnodesize = "auto";
+                };
+                options = {
+                    ashift = "12";
+                    autotrim = "on";
+                };
+
+                datasets = {
+                    "local" = {
+                        type = "zfs_fs";
+                        options.mountpoint = "none";
+                    };
+                    "local/root" = {
+                        type = "zfs_fs";
+                        mountpoint = "/home/${username}/Documents/HDD";
+                        options."com.sun:auto-snapshot" = "false";
+                    };
+                };
+            };
+        };
+    };
 }
