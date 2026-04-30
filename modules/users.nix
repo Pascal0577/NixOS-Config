6896f17df4f27e8619a7b4a061927792dd47d2cf
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-04-30 00:27:54 -0500

    ensure a password is always set

--- modules/users.nix
+++ modules/users.nix
@@ -2,6 +2,7 @@
 
 {
     users.users.${username} = {
+        hashedPasswordFile = "/nix/persist/passwd/${username}";
         isNormalUser = true;
         description = "Pascal";
         extraGroups = [ "networkmanager" "wheel" ];
@@ -23,5 +24,44 @@
                 stateVersion = "26.05";
             };
         };
+    };
+
+    # Make sure we always have a password
+    system.activationScripts.passwordInit = {
+        deps = [ "users" "groups" ];
+        text = ''
+            PASSWORD_FILE="/nix/persist/passwd/${username}"
+            TARGET_USER=${username}
+
+            if [ ! -f "$PASSWORD_FILE" ]; then
+                echo ""
+                echo "──────────────────────────────────────────────────────"
+                echo "  Password creation for user: $TARGET_USER"
+                echo "  No password file found at: $PASSWORD_FILE"
+                echo "──────────────────────────────────────────────────────"
+
+                mkdir -p "$(dirname "$PASSWORD_FILE")"
+
+                while true; do
+                    HASHED="$(${pkgs.mkpasswd}/bin/mkpasswd \
+                        --method=yescrypt \
+                        --rounds=3)" && break
+                    echo "Password failed or cancelled. Try again" >&2
+                done
+
+                printf '%s\n' "$HASHED" > "$PASSWORD_FILE"
+                chmod 0400 "$PASSWORD_FILE"
+
+                printf '%s:%s\n' "$TARGET_USER" "$HASHED" | \
+                    ${pkgs.shadow}/bin/chpasswd --encrypted
+
+                printf 'root:%s\n' "$HASHED" | \
+                    ${pkgs.shadow}/bin/chpasswd --encrypted
+
+                echo "Password set and hash stored at $PASSWORD_FILE"
+                echo ""
+                unset $HASHED
+            fi
+        '';
     };
 }
