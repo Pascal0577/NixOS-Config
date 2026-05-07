a5ad0d9d6c3b806a28531c32cca161e4c58b9670
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-05-07 02:39:16 -0500

    fix git configuration

--- modules/applications/git.nix
+++ modules/applications/git.nix
@@ -6,11 +6,13 @@
 
     config = lib.mkIf config.mySystem.applications.git.enable {
         environment.systemPackages = [ pkgs.gh ];
-        programs.git.enable = true;
 
-        home-manager.users.${username}.programs.git.settings = {
-            user.email = "181921662+Pascal0577@users.noreply.github.com";
-            user.name = "Pascal0577";
+        home-manager.users.${username}.programs.git = {
+            enable = true;
+            settings = {
+                user.email = "181921662+Pascal0577@users.noreply.github.com";
+                user.name = "Pascal0577";
+            };
         };
     };
 }
