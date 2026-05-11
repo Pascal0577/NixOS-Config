95941cd9693ce3de0a4054b66fadc36051ed09ca
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-05-11 01:20:17 -0400

    add chronos system

---
+++ systems/chronos/default.nix
@@ -0,0 +1,15 @@
+{
+    mySystem = {
+        desktop.oxwm.enable = true;
+        theme.everforest.enable = true;
+        applications = {
+            terminal.alacritty.enable = true;
+            file-manager.yazi.enable = true;
+            launcher.dmenu.enable = true;
+            helix.enable = true;
+            heroic.enable = false;
+            mathematica.enable = false;
+            virt-manager.enable = false;
+        };
+    };
+}
---
+++ systems/chronos/hardware-configuration.nix
@@ -0,0 +1,71 @@
+{ inputs, ... }:
+
+{
+    imports = [ inputs.disko.nixosModules.disko ];
+
+    nixpkgs.hostPlatform = "x86_64-linux";
+    hardware.enableRedistributableFirmware = false;
+
+    boot = {
+        initrd.systemd.enable = true;
+        loader.systemd-boot.enable = true;
+        
+        kernelParams = [
+            "amdgpu.dc=0"
+            "iommu=soft"
+            "rootflags=noflush_merge"
+        ];
+
+        initrd.kernelModules = [
+            "f2fs"
+            "fam15h_power"
+            "mmc_block"
+            "mmc_core"
+            "rpmb-core"
+            "sdhci_pci"
+        ];
+    };
+
+    disko.devices = {
+        disk.main = {
+            type = "disk";
+            device = "/dev/disk/by-id/mmc-MMC32G_0x00136833";
+            content = {
+                type = "gpt";
+                partitions = {
+                    ESP = {
+                        size = "1G";
+                        type = "EF00";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
+                        };
+                    };
+
+                    swap = {
+                        size = "4G";
+                        content.type = "swap";
+                    };
+
+                    root = {
+                        size = "100%";
+                        content = {
+                            type = "filesystem";
+                            format = "f2fs";
+                            mountpoint = "/";
+                            mountOptions = [
+                                "compress_algorithm=lz4"
+                                "compress_chksum"
+                                "atgc"
+                                "gc_merge"
+                                "lazytime"
+                            ];
+                        };
+                    };
+                };
+            };
+        };
+    };
+}
