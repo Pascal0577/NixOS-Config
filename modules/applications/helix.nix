80c65de9eaf6767d0c81ae469522c10812c46d01
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-05-19 23:42:41 -0400

    fix helix stylix declaration

--- modules/applications/helix.nix
+++ modules/applications/helix.nix
@@ -5,33 +5,35 @@
 
     config = lib.mkIf config.mySystem.applications.helix.enable {
         environment.sessionVariables = { EDITOR = "hx"; };
-        home-manager.users.${username}.programs.helix = {
+        home-manager.users.${username} = {
             stylix.targets.helix.enable = true;
-            enable = true;
-            defaultEditor = true;
-            extraPackages = [ pkgs.nixd ];
-            settings = {
-                editor = {
-                    line-number = "relative";
-                    indent-guides = {
-                        render = true;
-                        character = "┊";
-                    };
-                    cursor-shape = {
-                        normal = "block";
-                        insert = "bar";
-                        select = "underline";
+            programs.helix = {
+                enable = true;
+                defaultEditor = true;
+                extraPackages = [ pkgs.nixd ];
+                settings = {
+                    editor = {
+                        line-number = "relative";
+                        indent-guides = {
+                            render = true;
+                            character = "┊";
+                        };
+                        cursor-shape = {
+                            normal = "block";
+                            insert = "bar";
+                            select = "underline";
+                        };
                     };
                 };
-            };
-            languages = {
-                language = map (l: {
-                    name = "${l}";
-                    indent = {
-                        tab-width = 4;
-                        unit = "    ";
-                    };
-                }) [ "nix" "zig" "bash" ];
+                languages = {
+                    language = map (l: {
+                        name = "${l}";
+                        indent = {
+                            tab-width = 4;
+                            unit = "    ";
+                        };
+                    }) [ "nix" "zig" "bash" ];
+                };
             };
         };
     };
