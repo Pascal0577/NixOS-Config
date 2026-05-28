9021aae352e2d2e579363fd5e5e23f6ff81e1ac2
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-05-28 14:26:45 -0400

    add nv-run alias to zshrc

--- modules/applications/zsh.nix
+++ modules/applications/zsh.nix
@@ -50,6 +50,7 @@
                         lsl = "eza --color=always --icons=always --group-directories-first -lAh --total-size";
                         nivm = "nvim";
                         run0 = "run0 --background=";
+                        nv-run = "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only GBM_BACKEND=nvidia-drm";
                     };
 
                     completionInit = "";
