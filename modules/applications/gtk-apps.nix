90684f06fd0ef2d0855eba121808c662e1bb4bd8
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-06-18 11:59:04 -0400

    better way of handling default apps

--- modules/applications/gtk-apps.nix
+++ modules/applications/gtk-apps.nix
@@ -5,126 +5,253 @@
         lib.mkEnableOption "Common GTK apps module"
         // { default = !config.mySystem.server.enable; };
 
-    config = lib.mkIf config.mySystem.applications.gtk-apps.enable {
+    config = let
+        imageViewer = "org.gnome.Loupe.desktop";
+        videoPlayer = "io.github.celluloid_player.Celluloid.desktop";
+        audioPlayer = "org.gnome.Decibels.desktop";
+        webBrowser = "zen-beta.desktop";
+
+        imageMimes = [
+            "image/apng"
+            "image/avif"
+            "image/avif-sequence"
+            "image/bmp"
+            "image/cgm"
+            "image/g3fax"
+            "image/gif"
+            "image/heic"
+            "image/ief"
+            "image/jp2"
+            "image/jpeg"
+            "image/jxl"
+            "image/pjpeg"
+            "image/png"
+            "image/prs.btif"
+            "image/svg+xml"
+            "image/svg+xml-compressed"
+            "image/qoi"
+            "image/tiff"
+            "image/vnd.fpx"
+            "image/vnd.microsoft.icon"
+            "image/vnd.wap.wbmp"
+            "image/vnd.xiff"
+            "image/webp"
+            "image/x-adobe-dng"
+            "image/x-canon-cr2"
+            "image/x-canon-crw"
+            "image/x-cmu-raster"
+            "image/x-cmx"
+            "image/x-dds"
+            "image/x-epson-erf"
+            "image/x-exr"
+            "image/x-freehand"
+            "image/x-fuji-raf"
+            "image/x-icns"
+            "image/x-icon"
+            "image/x-kodak-dcr"
+            "image/x-kodak-k25"
+            "image/x-kodak-kdc"
+            "image/x-minolta-mrw"
+            "image/x-nikon-nef"
+            "image/x-olympus-orf"
+            "image/x-panasonic-raw"
+            "image/x-pcx"
+            "image/x-pentax-pef"
+            "image/x-pict"
+            "image/x-portable-anymap"
+            "image/x-portable-bitmap"
+            "image/x-portable-graymap"
+            "image/x-portable-pixmap"
+            "image/x-qoi"
+            "image/x-rgb"
+            "image/x-sigma-x3f"
+            "image/x-sony-arw"
+            "image/x-sony-sr2"
+            "image/x-sony-srf"
+            "image/x-tga"
+            "image/x-win-bitmap"
+            "image/x-xbitmap"
+            "image/x-xpixmap"
+            "image/x-xwindowdump"
+        ];
+
+        imageBindings = lib.genAttrs imageMimes (_: imageViewer);
+
+        videoMimes = [
+            "application/mpeg4-iod"
+            "application/mpeg4-muxcodetable"
+            "application/mxf"
+            "application/ram"
+            "application/sdp"
+            "application/streamingmedia"
+            "application/vnd.apple.mpegurl"
+            "application/vnd.ms-asf"
+            "application/vnd.rn-realmedia"
+            "application/vnd.rn-realmedia-vbr"
+            "application/x-extension-mp4"
+            "application/x-flash-video"
+            "application/x-matroska"
+            "application/x-streamingmedia"
+            "video/3gp"
+            "video/3gpp"
+            "video/3gpp2"
+            "video/divx"
+            "video/dv"
+            "video/fli"
+            "video/flv"
+            "video/mp2t"
+            "video/mp4"
+            "video/mp4v-es"
+            "video/mpeg"
+            "video/mpeg-system"
+            "video/msvideo"
+            "video/ogg"
+            "video/quicktime"
+            "video/vnd.mpegurl"
+            "video/vnd.rn-realvideo"
+            "video/webm"
+            "video/x-avi"
+            "video/x-flc"
+            "video/x-fli"
+            "video/x-flv"
+            "video/x-m4v"
+            "video/x-matroska"
+            "video/x-mpeg"
+            "video/x-mpeg-system"
+            "video/x-mpeg2"
+            "video/x-ms-asf"
+            "video/x-ms-wm"
+            "video/x-ms-wmv"
+            "video/x-ms-wmx"
+            "video/x-msvideo"
+            "video/x-nsv"
+            "video/x-ogm+ogg"
+            "video/x-theora"
+            "video/x-theora+ogg"
+            "x-content/video-dvd"
+            "x-scheme-handler/mms"
+            "x-scheme-handler/mmsh"
+            "x-scheme-handler/rtmp"
+            "x-scheme-handler/rtp"
+            "x-scheme-handler/rtsp"
+        ];
+
+        videoBindings = lib.genAttrs videoMimes (_: videoPlayer);
+
+        audioMimes = [
+            "application/ogg"
+            "application/x-extension-m4a"
+            "application/x-flac"
+            "application/x-ogg"
+            "audio/3gpp"
+            "audio/3gpp2"
+            "audio/aac"
+            "audio/aacp"
+            "audio/ac3"
+            "audio/adpcm"
+            "audio/aiff"
+            "audio/amr"
+            "audio/amr-wb"
+            "audio/basic"
+            "audio/dv"
+            "audio/eac3"
+            "audio/flac"
+            "audio/m4a"
+            "audio/midi"
+            "audio/mp1"
+            "audio/mp2"
+            "audio/mp3"
+            "audio/mp4"
+            "audio/mp4a-latm"
+            "audio/mpeg"
+            "audio/mpegurl"
+            "audio/mpg"
+            "audio/ogg"
+            "audio/opus"
+            "audio/scpls"
+            "audio/vnd.dolby.heaac.1"
+            "audio/vnd.dolby.heaac.2"
+            "audio/vnd.dolby.mlp"
+            "audio/vnd.dts"
+            "audio/vnd.dts.hd"
+            "audio/vnd.rn-realaudio"
+            "audio/vnd.wav"
+            "audio/wav"
+            "audio/wave"
+            "audio/webm"
+            "audio/x-aac"
+            "audio/x-aiff"
+            "audio/x-ape"
+            "audio/x-flac"
+            "audio/x-gsm"
+            "audio/x-it"
+            "audio/x-m4a"
+            "audio/x-m4b"
+            "audio/x-matroska"
+            "audio/x-mod"
+            "audio/x-mp1"
+            "audio/x-mp2"
+            "audio/x-mp3"
+            "audio/x-mpg"
+            "audio/x-mpeg"
+            "audio/x-mpegurl"
+            "audio/x-mpg"
+            "audio/x-ms-asf"
+            "audio/x-ms-wax"
+            "audio/x-ms-wma"
+            "audio/x-musepack"
+            "audio/x-opus+ogg"
+            "audio/x-pn-aiff"
+            "audio/x-pn-au"
+            "audio/x-pn-realaudio"
+            "audio/x-pn-wav"
+            "audio/x-real-audio"
+            "audio/x-realaudio"
+            "audio/x-s3m"
+            "audio/x-scpls"
+            "audio/x-shorten"
+            "audio/x-speex"
+            "audio/x-tta"
+            "audio/x-vorbis"
+            "audio/x-vorbis+ogg"
+            "audio/x-wav"
+            "audio/x-wavpack"
+            "audio/x-xm"
+            "x-content/audio-cdda"
+            "x-content/audio-player"
+        ];
+
+        audioBindings = lib.genAttrs audioMimes (_: audioPlayer);
+
+        browserMimes = [
+            "application/xhtml+xml"
+            "message/rfc822"
+            "text/html"
+            "x-scheme-handler/http"
+            "x-scheme-handler/https"
+            "x-scheme-handler/about"
+            "x-scheme-handler/unknown"
+        ];
+
+        browserBindings = lib.genAttrs browserMimes (_: webBrowser);
+    in lib.mkIf config.mySystem.applications.gtk-apps.enable {
         environment.sessionVariables = { GSK_RENDERER = "gl"; };
         environment.systemPackages = with pkgs; [
+            loupe
+            celluloid
+            decibels
             baobab
-            totem
             constrict
             curtail
             deluge
             pinta
             blanket
             video-trimmer
-            loupe
         ];
 
-        xdg.mime.defaultApplications = {
-            "video/mkv" = "org.gnome.Totem.desktop";
-            "application/mxf" = "org.gnome.Totem.desktop";
-            "application/ram" = "org.gnome.Totem.desktop";
-            "application/sdp" = "org.gnome.Totem.desktop";
-            "application/vnd.ms-asf" = "org.gnome.Totem.desktop";
-            "application/vnd.ms-wpl" = "org.gnome.Totem.desktop";
-            "application/vnd.rn-realmedia" = "org.gnome.Totem.desktop";
-            "application/vnd.rn-realmedia-vbr" = "org.gnome.Totem.desktop";
-            "application/x-extension-m4a" = "org.gnome.Totem.desktop";
-            "application/x-extension-mp4" = "org.gnome.Totem.desktop";
-            "application/x-flash-video" = "org.gnome.Totem.desktop";
-            "application/x-matroska" = "org.gnome.Totem.desktop";
-            "application/x-netshow-channel" = "org.gnome.Totem.desktop";
-            "application/x-quicktimeplayer" = "org.gnome.Totem.desktop";
-            "application/x-shorten" = "org.gnome.Totem.desktop";
-            "image/vnd.rn-realpix" = "org.gnome.Totem.desktop";
-            "image/x-pict" = "org.gnome.Totem.desktop";
-            "misc/ultravox" = "org.gnome.Totem.desktop";
-            "text/x-google-video-pointer" = "org.gnome.Totem.desktop";
-            "video/3gp" = "org.gnome.Totem.desktop";
-            "video/3gpp" = "org.gnome.Totem.desktop";
-            "video/3gpp2" = "org.gnome.Totem.desktop";
-            "video/dv" = "org.gnome.Totem.desktop";
-            "video/divx" = "org.gnome.Totem.desktop";
-            "video/fli" = "org.gnome.Totem.desktop";
-            "video/flv" = "org.gnome.Totem.desktop";
-            "video/mp2t" = "org.gnome.Totem.desktop";
-            "video/mp4" = "org.gnome.Totem.desktop";
-            "video/mp4v-es" = "org.gnome.Totem.desktop";
-            "video/mpeg" = "org.gnome.Totem.desktop";
-            "video/mpeg-system" = "org.gnome.Totem.desktop";
-            "video/msvideo" = "org.gnome.Totem.desktop";
-            "video/ogg" = "org.gnome.Totem.desktop";
-            "video/quicktime" = "org.gnome.Totem.desktop";
-            "video/vivo" = "org.gnome.Totem.desktop";
-            "video/vnd.avi" = "org.gnome.Totem.desktop";
-            "video/vnd.divx" = "org.gnome.Totem.desktop";
-            "video/vnd.rn-realvideo" = "org.gnome.Totem.desktop";
-            "video/vnd.vivo" = "org.gnome.Totem.desktop";
-            "video/webm" = "org.gnome.Totem.desktop";
-            "video/x-anim" = "org.gnome.Totem.desktop";
-            "video/x-avi" = "org.gnome.Totem.desktop";
-            "video/x-flc" = "org.gnome.Totem.desktop";
-            "video/x-fli" = "org.gnome.Totem.desktop";
-            "video/x-flic" = "org.gnome.Totem.desktop";
-            "video/x-flv" = "org.gnome.Totem.desktop";
-            "video/x-m4v" = "org.gnome.Totem.desktop";
-            "video/x-matroska" = "org.gnome.Totem.desktop";
-            "video/x-mjpeg" = "org.gnome.Totem.desktop";
-            "video/x-mpeg" = "org.gnome.Totem.desktop";
-            "video/x-mpeg2" = "org.gnome.Totem.desktop";
-            "video/x-ms-asf" = "org.gnome.Totem.desktop";
-            "video/x-ms-asf-plugin" = "org.gnome.Totem.desktop";
-            "video/x-ms-asx" = "org.gnome.Totem.desktop";
-            "video/x-msvideo" = "org.gnome.Totem.desktop";
-            "video/x-ms-wm" = "org.gnome.Totem.desktop";
-            "video/x-ms-wmv" = "org.gnome.Totem.desktop";
-            "video/x-ms-wmx" = "org.gnome.Totem.desktop";
-            "video/x-ms-wvx" = "org.gnome.Totem.desktop";
-            "video/x-nsv" = "org.gnome.Totem.desktop";
-            "video/x-ogm+ogg" = "org.gnome.Totem.desktop";
-            "video/x-theora" = "org.gnome.Totem.desktop";
-            "video/x-theora+ogg" = "org.gnome.Totem.desktop";
-            "video/x-totem-stream" = "org.gnome.Totem.desktop";
-            "audio/x-pn-realaudio" = "org.gnome.Totem.desktop";
-            "application/smil" = "org.gnome.Totem.desktop";
-            "application/smil+xml" = "org.gnome.Totem.desktop";
-            "application/x-quicktime-media-link" = "org.gnome.Totem.desktop";
-            "application/x-smil" = "org.gnome.Totem.desktop";
-            "text/google-video-pointer" = "org.gnome.Totem.desktop";
-            "x-content/video-dvd" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/pnm" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/mms" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/net" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/rtp" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/rtmp" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/rtsp" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/mmsh" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/uvox" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/icy" = "org.gnome.Totem.desktop";
-            "x-scheme-handler/icyx" = "org.gnome.Totem.desktop";
-
-            "image/jpeg" = "org.gnome.Loupe.desktop";
-            "image/png" = "org.gnome.Loupe.desktop";
-            "image/gif" = "org.gnome.Loupe.desktop";
-            "image/webp" = "org.gnome.Loupe.desktop";
-            "image/tiff" = "org.gnome.Loupe.desktop";
-            "image/x-tga" = "org.gnome.Loupe.desktop";
-            "image/vnd-ms.dds" = "org.gnome.Loupe.desktop";
-            "image/x-dds" = "org.gnome.Loupe.desktop";
-            "image/bmp" = "org.gnome.Loupe.desktop";
-            "image/vnd.microsoft.icon" = "org.gnome.Loupe.desktop";
-            "image/vnd.radiance" = "org.gnome.Loupe.desktop";
-            "image/x-exr" = "org.gnome.Loupe.desktop";
-            "image/x-portable-bitmap" = "org.gnome.Loupe.desktop";
-            "image/x-portable-graymap" = "org.gnome.Loupe.desktop";
-            "image/x-portable-pixmap" = "org.gnome.Loupe.desktop";
-            "image/x-portable-anymap" = "org.gnome.Loupe.desktop";
-            "image/x-qoi" = "org.gnome.Loupe.desktop";
-            "image/qoi" = "org.gnome.Loupe.desktop";
-            "image/svg+xml" = "org.gnome.Loupe.desktop";
-            "image/svg+xml-compressed" = "org.gnome.Loupe.desktop";
-            "image/avif" = "org.gnome.Loupe.desktop";
-            "image/heic" = "org.gnome.Loupe.desktop";
-            "image/jxl" = "org.gnome.Loupe.desktop";
-        };
+        xdg.mime.defaultApplications = {} //
+            imageBindings //
+            videoBindings //
+            audioBindings;
     };
 }
