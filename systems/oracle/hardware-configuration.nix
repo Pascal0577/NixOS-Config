26dcc7413fc5cf0876fc5d7e2104924497dbd12c
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-06-27 14:23:35 -0400

    reduce indents

--- systems/oracle/hardware-configuration.nix
+++ systems/oracle/hardware-configuration.nix
@@ -23,70 +23,66 @@
     };
 
     disko.devices = {
-        disk = {
-            main = {
-                type = "disk";
-                device = "/dev/sda";
-                content = {
-                    type = "gpt";
-                    partitions = {
-                        ESP = {
-                            size = "1G";
-                            type = "EF00";
-                            content = {
-                                type = "filesystem";
-                                format = "vfat";
-                                mountpoint = "/boot";
-                                mountOptions = [ "umask=0077" ];
-                            };
+        disk.main = {
+            type = "disk";
+            device = "/dev/sda";
+            content = {
+                type = "gpt";
+                partitions = {
+                    ESP = {
+                        size = "1G";
+                        type = "EF00";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
                         };
-                        root = {
-                            size = "100%";
-                            content = {
-                                type = "zfs";
-                                pool = "zroot";
-                            };
+                    };
+                    root = {
+                        size = "100%";
+                        content = {
+                            type = "zfs";
+                            pool = "zroot";
                         };
                     };
                 };
             };
         };
-        zpool = {
-            zroot = {
-                type = "zpool";
-                rootFsOptions = {
-                    acltype = "posixacl";
-                    atime = "off";
-                    compression = "lz4";
-                    mountpoint = "none";
-                    xattr = "sa";
-                    dnodesize = "auto";
-                };
-                options = {
-                    ashift = "12";
-                    autotrim = "on";
-                };
+        zpool.zroot = {
+            type = "zpool";
+            rootFsOptions = {
+                acltype = "posixacl";
+                atime = "off";
+                compression = "lz4";
+                mountpoint = "none";
+                xattr = "sa";
+                dnodesize = "auto";
+            };
+            options = {
+                ashift = "12";
+                autotrim = "on";
+            };
 
-                datasets = {
-                    "local" = {
-                        type = "zfs_fs";
-                        options.mountpoint = "none";
-                    };
-                    "local/nix" = {
-                        type = "zfs_fs";
-                        mountpoint = "/nix";
-                        # Nix store has lots of small files
-                        options.recordsize = "64K";
-                    };
-                    "local/persist" = {
-                        type = "zfs_fs";
-                        mountpoint = "/nix/persist";
-                    };
-                    "local/root" = {
-                        type = "zfs_fs";
-                        mountpoint = "/";
-                        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank";
-                    };
+            datasets = {
+                "local" = {
+                    type = "zfs_fs";
+                    options.mountpoint = "none";
+                };
+                "local/nix" = {
+                    type = "zfs_fs";
+                    mountpoint = "/nix";
+                    # Nix store has lots of small files
+                    options.recordsize = "64K";
+                };
+                "local/persist" = {
+                    type = "zfs_fs";
+                    mountpoint = "/nix/persist";
+                };
+                "local/root" = {
+                    type = "zfs_fs";
+                    mountpoint = "/";
+                    postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank";
                 };
             };
         };
