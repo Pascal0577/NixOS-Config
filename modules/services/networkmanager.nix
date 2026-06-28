8bf3f99a09d807b6c4fccb112a8b1cdaa7b1ea5c
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-06-27 20:27:33 -0400

    use hardening function for NetworkManager

--- modules/services/networkmanager.nix
+++ modules/services/networkmanager.nix
@@ -1,15 +1,7 @@
+{ hardening, ... }:
+
 {
-    systemd.services.NetworkManager.serviceConfig = {
-        NoNewPrivileges = true;
-        ProtectHome = true;
-        ProtectKernelModules = true;
-        ProtectKernelLogs = true;
-        ProtectControlGroups = true;
-        ProtectClock = true; 
-        ProtectHostname = true;
-        ProtectProc = "invisible";
-        PrivateTmp = true;
-        RestrictRealtime = true;
+    systemd.services.NetworkManager.serviceConfig = hardening.mkService {
         RestrictAddressFamilies = [ 
             "AF_UNIX" 
             "AF_NETLINK"
@@ -17,9 +9,6 @@
             "AF_INET6"
             "AF_PACKET"
         ];
-        RestrictNamespaces = true;
-        RestrictSUIDSGID = true;
-        MemoryDenyWriteExecute = true;
         SystemCallFilter = [
             "~@mount"
             "~@module"
@@ -28,7 +17,5 @@
             "~@cpu-emulation" 
             "ptrace"
         ];
-        SystemCallArchitectures = "native";
-        LockPersonality= true; 
     };
 }
