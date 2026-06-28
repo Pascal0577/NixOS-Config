b57e09cbc249c4a7b763ebe923eadbdb08bf9092
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-06-27 20:28:59 -0400

    use hardening function for systemd-rfkill

--- modules/services/systemd-rfkill.nix
+++ modules/services/systemd-rfkill.nix
@@ -1,29 +1,7 @@
+{ hardening, ... }:
+
 {
-    systemd.services.systemd-rfkill.serviceConfig = {
-        NoNewPrivileges = true;
-        ProtectSystem = "strict";
-        ProtectHome = true;
-        ProtectKernelLogs = true;
-        ProtectControlGroups = true;
-        ProtectClock = true; 
-        ProtectHostname = true;
-        ProtectProc = "invisible";
-        PrivateTmp = true;
-        PrivateNetwork = true;
-        PrivateUsers = true;
-        RestrictNamespaces = true;
-        RestrictRealtime = true;
-        RestrictAddressFamilies = [ "AF_UNIX" ];
-        RestrictSUIDSGID = true;
-        MemoryDenyWriteExecute = true;
-        SystemCallFilter = [
-            "~@swap"
-            "~@obsolete"
-            "~@cpu-emulation"
-            "~@privileged"
-        ];
-        SystemCallArchitectures = "native";
-        LockPersonality = true;
+    systemd.services.systemd-rfkill.serviceConfig = hardening.mkService {
         CapabilityBoundingSet = [
             "~CAP_SYS_PTRACE" 
             "~CAP_SYS_PACCT"
