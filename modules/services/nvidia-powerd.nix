25f0ccebf8f9f6df50f807c219e28cf9dfb64a77
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-06-27 20:11:15 -0400

    use hardening function for nvidia-powerd

--- modules/services/nvidia-powerd.nix
+++ modules/services/nvidia-powerd.nix
@@ -1,38 +1,12 @@
+{ hardening, ... }:
+
 {
-    systemd.services.nvidia-powerd.serviceConfig = {
-        NoNewPrivileges = true;
-        ProtectHome = true;
-        ProtectHostname = true;
-        ProtectKernelLogs = true;
-        ProtectClock = true;
-        ProtectControlGroups = true;
-        PrivateTmp = true;
+    systemd.services.nvidia-powerd.serviceConfig = hardening.mkService {
         PrivateNetwork = true;
-        ProtectProc = "invisible";
-        ProtectSystem = "strict";
-        RestrictSUIDSGID = true;
-        RestrictRealtime = true;
-        RestrictNamespaces = true;
-        LockPersonality = true;
-        SystemCallArchitectures = "native";
-        SystemCallFilter = [
-            "~@clock"
-            "~@cpu-emulation"
-            "~@debug"
-            "~@module"
-            "~@obsolete"
-            "~@reboot"
-            "~@swap"
-        ];
-        RestrictAddressFamilies = [
-            "AF_UNIX"
-            "AF_NETLINK"
-        ];
-        CapabilityBoundingSet = [
-            "~CAP_SETUID"
-            "~CAP_SETGID"
-            "~CAP_SETPCAP"
-        ];
-        UMask = "0077";
+        RestrictAddressFamilies = [ "AF_UNIX" "AF_NETLINK" ];
+
+        ProtectKernelTunables = false;
+        ProtectKernelModules = false;
+        SystemCallFilter = hardening.defaultProfile.SystemCallFilter ++ [ "@module" ];
     };
 }
